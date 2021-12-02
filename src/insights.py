5c590fcaff7e4c38858c4c984e852650e4bea172
jaarguello <johnny.arguello007@gmail.com>
2021-12-02 17:52:58 -0300

    requirement 2

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+import csv
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,14 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+
+    with open(path) as file:
+        result = set()
+        jobs = csv.DictReader(file)
+        for job in jobs:
+            result.add(job["job_type"])
+
+    return result
 
 
 def filter_by_job_type(jobs, job_type):
