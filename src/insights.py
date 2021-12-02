351df3582abb4f3e75041b0b801a355908030e2e
EderPaiva <edergomesdepaiva12@gmail.com>
2021-12-02 18:07:19 -0300

    requisito 6 ?

--- src/insights.py
+++ src/insights.py
@@ -11,21 +11,10 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    filtered_jobs = [job for job in jobs if job["job_type"] == job_type]
+    return filtered_jobs
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+# Assim que faz um filter
 
 
 def get_unique_industries(path):
