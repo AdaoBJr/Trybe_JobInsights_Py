0f06ff6daa4f31fe357930a955e7e3cb18f9b842
Leandro Reis <lleandrofr@gmail.com>
2021-12-02 20:09:33 -0300

    req1: implements read function

--- src/jobs.py
+++ src/jobs.py
@@ -1,6 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    return []
+    jobs_dict = []
+    with open(path) as file:
+        jobs_reader = csv.DictReader(file)
+        for dict in jobs_reader:
+            jobs_dict.append(dict)
+        return jobs_dict
