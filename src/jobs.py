4a7970a85cdcc40132fe256fcf83e2d469777127
Lara-Capila <laracapilak@gmail.com>
2021-12-02 15:59:56 -0300

    feat(req01): Requisito 01 passando

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,11 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    with open(path) as job_file:
+        return list(csv.DictReader(job_file))
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+read("src/jobs.csv")
