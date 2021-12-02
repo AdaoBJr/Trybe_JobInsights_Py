199d409f96d4fd3555c354cd58221d7a632c0e28
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-12-02 15:56:51 -0300

    feat: get_min_salary
    retorna o menor salário da lista

--- src/insights.py
+++ src/insights.py
@@ -64,21 +64,12 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    min_salaries = []
+    jobs_list = read(path)
+    for job in jobs_list:
+        if job["min_salary"].isnumeric():
+            min_salaries.append(int(job["min_salary"]))
+    return min(min_salaries)
 
 
 def matches_salary_range(job, salary):
