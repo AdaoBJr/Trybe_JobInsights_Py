5404af42a658acb5b646d21135cd5ada45240461
denis-rossati <denis.rossatiramos@gmail.com>
2021-12-02 16:10:04 -0300

    creating filter_by_salary_range

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    result = []
+    for job in jobs:
+        if job['industry'] == industry:
+            result.append(job)
+    return result
 
 
 def get_max_salary(path):
@@ -77,45 +67,46 @@ def get_min_salary(path):
     return lesser_salary
 
 
+def validate_max_and_mix_salary(job):
+    if 'min_salary' not in job or 'max_salary' not in job:
+        # https://rollbar.com/blog/throwing-exceptions-in-python/
+        raise ValueError('Job needs "min_salary" and "max_salary" keys')
+
+    if (
+        not isinstance(job['min_salary'], int)
+        or not isinstance(job['max_salary'], int)
+    ):
+        raise ValueError('Job keys and salary have to be a int')
+
+    if job['min_salary'] > job['max_salary']:
+        raise ValueError('Min. salary cannot be bigger than max. salary')
+
+
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    validate_max_and_mix_salary(job)
+    if (not isinstance(salary, int)):
+        raise ValueError('Job keys and salary have to be a int')
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if job['min_salary'] <= salary <= job['max_salary']:
+        return True
+    else:
+        return False
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
+    works = []
+    matched_salary = []
+    for job in jobs:
+        if (
+            job['min_salary'] > -1
+            and job['max_salary'] > -1
+            and job['min_salary'] < job['max_salary']
+            and isinstance(salary, int)
+        ):
+            works.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
+    for job in works:
+        if matches_salary_range(job, salary):
+            matched_salary.append(job)
 
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    return matched_salary
