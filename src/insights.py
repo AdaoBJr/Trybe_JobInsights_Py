7bb92d6a336e9a47830b3c6ca66fe7d7fff25cd7
Lara-Capila <laracapilak@gmail.com>
2021-12-02 20:09:51 -0300

    feat(req06): Requisito 06 passando - implementa filter_by_job_type

--- src/insights.py
+++ src/insights.py
@@ -16,21 +16,11 @@ print(get_unique_job_types("src/jobs.csv"))
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    list = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            list.append(job)
+    return list
 
 
 def get_unique_industries(path):
