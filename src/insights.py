7517b5cc67f7a6be8fd0639ae63977918324bf4b
Huggo Parcelly <h.parcelly@gmail.com>
2021-12-02 16:40:51 -0300

    adiciona a funcao que retorna os tipos unicos de industrias

--- src/insights.py
+++ src/insights.py
@@ -57,7 +57,11 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    all_jobs = read(path)
+
+    industries_types = [job["industry"] for job in all_jobs if job["industry"]]
+    unique_industry_type = list(set(industries_types))
+    return unique_industry_type
 
 
 def filter_by_industry(jobs, industry):
