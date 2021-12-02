66c16f9faa4c7769e8cd20114159c3814c1a29c9
denis-rossati <denis.rossatiramos@gmail.com>
2021-12-02 14:17:35 -0300

    creating get_unique_job_types function

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,14 @@
+from jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
+    file = read(path)
+    unique_jobs = set()
+    for job in file:
+        unique_jobs.add(job['job_title'])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    # https://www.geeksforgeeks.org/python-convert-set-into-a-list/
+    return list(unique_jobs)
 
 
 def filter_by_job_type(jobs, job_type):
