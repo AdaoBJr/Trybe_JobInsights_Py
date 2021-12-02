e73f598be426791ba45b956a02bdcaf59c189c8f
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-12-02 19:19:12 -0300

    feat: test_sortying

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,25 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+jobs_list = [
+    {"min_salary": 2000, "max_salary": 6000, "date_posted": "2020-0101"},
+    {"min_salary": 1000, "max_salary": 5000, "date_posted": "2019-01-01"},
+    {"min_salary": 3000, "max_salary": 7000, "date_posted": "2021-01-01"},
+]
+
+jobs_list_min = [
+    {"min_salary": 1000, "max_salary": 5000, "date_posted": "2019-01-01"},
+    {"min_salary": 2000, "max_salary": 6000, "date_posted": "2020-01-01"},
+    {"min_salary": 3000, "max_salary": 7000, "date_posted": "2021-01-01"},
+]
+
+jobs_list_max_and_date = [
+    {"min_salary": 3000, "max_salary": 7000, "date_posted": "2021-01-01"},
+    {"min_salary": 2000, "max_salary": 6000, "date_posted": "2020-01-01"},
+    {"min_salary": 1000, "max_salary": 5000, "date_posted": "2019-01-01"},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    assert sort_by(jobs_list, "min_salary") == jobs_list_min
+    assert sort_by(jobs_list, "max_salary") == jobs_list_max_and_date
+    assert sort_by(jobs_list, "date_posted") == jobs_list_max_and_date
