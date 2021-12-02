325da405de158b3738a9fd65823bc8f3bd71f0dc
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-12-02 20:08:53 -0300

    feat: :sparkles: implement req 09

--- src/insights.py
+++ src/insights.py
@@ -210,4 +210,15 @@ def filter_by_salary_range(jobs, salary):
     list
         Jobs whose salary range contains `salary`
     """
-    return []
+    """
+    Material consultado sobre como tratar o erro ao construir a lista
+    https://stackoverflow.com/a/1528286
+    """
+    filtered_jobs = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filtered_jobs.append(job)
+        except ValueError:
+            pass
+    return filtered_jobs
