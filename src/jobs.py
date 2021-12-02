bf39c5cd6db6e9a967d688dfd0abee27a73dfedf
dennisMarcelo <dennismarcelo15@gmail.com>
2021-12-02 18:23:44 -0300

    fixing lint errors

--- src/jobs.py
+++ src/jobs.py
@@ -1,12 +1,13 @@
 from functools import lru_cache
 import csv
 
+
 @lru_cache
 def read(path):
     with open(path, encoding='utf-8') as file:
-       data = []
-       csvReader = csv.DictReader(file)
-       for row in csvReader:
-        data.append(row)
-    
+        data = []
+        csvReader = csv.DictReader(file)
+        for row in csvReader:
+            data.append(row)
+
     return data
