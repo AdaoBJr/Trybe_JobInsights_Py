101e80b80610d184dfd2a3124894214f4e4ab1b7
Paulo Xavier <pxavierf@gmail.com>
2021-12-02 18:50:59 -0300

    req 7

--- src/insights.py
+++ src/insights.py
@@ -32,21 +32,13 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    job = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
+    for v in jobs:
+        if industry in v.values():
+            job.append(v)
 
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return job
 
 
 def get_max_salary(path):
