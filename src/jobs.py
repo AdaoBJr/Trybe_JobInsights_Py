cb278ff840178d219f07d96e42f725a7b31cc339
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-12-02 19:31:43 -0300

    Corrigido erro ao iniciar o projeto

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,17 @@
 from functools import lru_cache
-# iniciando o project-job-insights
+
 
 @lru_cache
 def read(path):
     """Reads a file from a given path and returns its contents
-
     Parameters
     ----------
     path : str
         Full path to file
-
     Returns
     -------
     list
         List of rows as dicts
+        Iniciando o project-job-insights
     """
     return []
