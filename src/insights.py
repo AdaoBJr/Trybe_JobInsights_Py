61667526069a36bc65248b1a3b9a5ed9c05af783
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-12-02 16:17:43 -0300

    feat: req 6 - feito
    filter_by_job_type

--- src/insights.py
+++ src/insights.py
@@ -6,39 +6,16 @@ def get_unique_job_types(path):
     lista = set()
     for row in data:
         lista.add(row["job_type"])
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
     return lista
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    list_job = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            list_job.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return list_job
 
 
 def get_unique_industries(path):
