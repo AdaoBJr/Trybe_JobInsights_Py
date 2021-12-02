e0b91b334345163ab066768cd287d8f394094b0f
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-12-02 16:36:56 -0300

    Inserir - insights - Criação da função que retorna somente a coluna jobs_title sem valores repetidos e sem espaços em branco

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,12 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    file = read(path)
+    jobs_title = set()
+    for job in file:
+        jobs_title.add(job['job_type'])
+    return list(jobs_title)
 
 
 def filter_by_job_type(jobs, job_type):
--- src/jobs.py
+++ src/jobs.py
@@ -4,24 +4,9 @@ import csv
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
     with open(path) as file:
         csv_file = csv.DictReader(file, delimiter=",", quotechar='"')
         jobs = []
         for job in list(csv_file):
             jobs.append(job)
         return jobs
-
-
-read("jobs.csv")
