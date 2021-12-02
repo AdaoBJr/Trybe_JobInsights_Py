ee77853cd7def87caad885eeddfc295502088db9
Antenor <antenorzapata@gmail.com>
2021-12-02 12:14:07 -0300

    feat: implement the filter_by_industry function

--- src/insights.py
+++ src/insights.py
@@ -33,21 +33,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    result = []
+    for val in jobs:
+        if(val['industry'] == industry):
+            result.append(val)
+    return result
 
 
 def get_max_salary(path):
