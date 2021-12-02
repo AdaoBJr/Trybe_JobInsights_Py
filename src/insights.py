a6ffd60a2b0f8a03bfa5edef93e6f5160d63fd0e
EderPaiva <edergomesdepaiva12@gmail.com>
2021-12-02 18:19:06 -0300

    dale dele dole

--- src/insights.py
+++ src/insights.py
@@ -18,21 +18,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    industries_list = read(path)
+    industry_list_header = set()
+    for industry in industries_list:
+        if industry["industry"] != '':
+            industry_list_header.add(industry["industry"])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return industry_list_header
 
 
 def filter_by_industry(jobs, industry):
