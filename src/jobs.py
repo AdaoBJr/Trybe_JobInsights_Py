b596435724825612ec85ea672fe1425567a12b58
Daniel <dsouzaborini@gmail.com>
2021-12-02 15:16:59 -0300

    fix req 1 function

--- src/jobs.py
+++ src/jobs.py
@@ -5,5 +5,8 @@ import csv
 @lru_cache
 def read(path):
     with open(path, 'r') as file:
-        jobs_list = list(csv.DictReader(file))
-        return jobs_list
+        content = csv.reader(file, delimiter=',')
+        content_list = []
+        for row in content:
+            content_list.append(dict(row))
+        return content_list
