d1afaf6f63deba52e4972a54c228243dd05c3eec
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-12-02 16:07:32 -0300

    feat: filter_by_industry
    retorna uma lista de industries filtrando através do industry

--- src/insights.py
+++ src/insights.py
@@ -27,21 +27,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    instrudies_list = []
+    for job in jobs:
+        if job["industry"] == industry:
+            instrudies_list.append(job)
+    return instrudies_list
 
 
 def get_max_salary(path):
