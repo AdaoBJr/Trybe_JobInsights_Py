f409296881e8b002c4cc58921b7c8f216b7b9840
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-12-02 15:56:20 -0300

    desafio 3 feito

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    list = read(path)
+    industries = set()  # set of unique industries
+    for industry in list:
+        if industry["industry"]:
+            industries.add(industry["industry"])
+    return industries
 
 
 def filter_by_industry(jobs, industry):
