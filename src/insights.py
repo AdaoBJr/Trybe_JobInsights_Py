6802bf4c75814b37268e536796be25033fdc3cbb
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-12-02 18:12:34 -0300

    feat(insights.py): Implementing get_unique_industries function
    - from read function in jobs module returns unique industries list

--- src/insights.py
+++ src/insights.py
@@ -52,7 +52,9 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    return list(
+        {item["industry"] for item in read(path) if item["industry"] not in ""}
+    )
 
 
 def filter_by_industry(jobs, industry):
