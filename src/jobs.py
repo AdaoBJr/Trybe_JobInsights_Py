9e02853285903baecfafa1727ef5ea40879c96fa
Joao Vanelli <vanellijoao@gmail.com>
2021-12-02 15:06:20 -0300

    Creating the Dict List with Jobs

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,15 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    jobs = []
+    with open(path) as file:
+        jobs_list = csv.DictReader(file)
+        for job in jobs_list:
+            jobs.append(job)
+        return jobs
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+print(read(path="src/jobs.csv"))
