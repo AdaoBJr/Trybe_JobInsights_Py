a2df3d07c83e9c0cb88b90e69da3042ddaad3191
legalvao1 <legalvao1@gmail.com>
2021-12-02 15:59:41 -0300

    Teste evaluator

--- src/jobs.py
+++ src/jobs.py
@@ -4,18 +4,6 @@ from functools import lru_cache
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
     with open(path) as file:
-        jobs = csv.DictReader(file)
+        jobs = csv.DictReader(file, delimiter=",", quotechar='"')
         return [job for job in jobs]
