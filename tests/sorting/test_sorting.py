a7ec2105411263de2f35ad8b2deb769947c4d3aa
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-12-03 09:07:34 -0300

    testando sorting(), max salary

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,24 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+data = [
+    {"min_salary": 3, "max_salary": 3, "date_posted": "03/01/2000"},
+    {"min_salary": 1, "max_salary": 1, "date_posted": "01/01/2000"},
+    {"min_salary": 2, "max_salary": 2, "date_posted": "02/01/2000"},
+]
+
+expected_min_salary = [
+    {"min_salary": 1, "max_salary": 1, "date_posted": "01/01/2000"},
+    {"min_salary": 2, "max_salary": 2, "date_posted": "01/01/2000"},
+    {"min_salary": 3, "max_salary": 3, "date_posted": "01/01/2000"},
+]
+
+expected_max_salary = [
+    {"min_salary": 3, "max_salary": 3, "date_posted": "03/01/2000"},
+    {"min_salary": 2, "max_salary": 2, "date_posted": "02/01/2000"},
+    {"min_salary": 1, "max_salary": 1, "date_posted": "01/01/2000"},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    report = sort_by(data, "max_salary")
+    assert expected_max_salary in report
