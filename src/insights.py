994c83e539fea032c2d3b14a9608aa1b0a839202
walterlbf <walter.lbf.f@gmail.com>
2021-12-03 14:21:50 -0400

    funcao get_unique_industries implementada e passando localmente

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    dict_from_jobs = read(path)
+    list_of_industries = set()
+    for industry in dict_from_jobs:
+        if industry["industry"]:
+            list_of_industries.add(industry["industry"])
+    return list_of_industries
 
 
 def filter_by_industry(jobs, industry):
