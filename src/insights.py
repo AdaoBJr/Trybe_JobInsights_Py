617510179a93a03de8b892f0ab4f630e433c42e6
Pedro Toselli <phtoselli@gmail.com>
2021-12-03 19:33:53 -0300

    Requisito 8

--- src/insights.py
+++ src/insights.py
@@ -1,4 +1,10 @@
 from src.jobs import read
+from src.validations import (
+    valid_keys_in_job,
+    valid_min_and_max_is_integer,
+    valid_min_is_greather_than_max,
+    valid_salary_is_an_integer,
+)
 
 
 def get_unique_job_types(path):
@@ -6,7 +12,7 @@ def get_unique_job_types(path):
     job_types = set()
 
     for item in array_of_jobs:
-        job_types.add(item['job_type'])
+        job_types.add(item["job_type"])
     return job_types
 
 
@@ -14,7 +20,7 @@ def filter_by_job_type(jobs, job_type):
     jobs_result = []
 
     for item in jobs:
-        if item['job_type'] == job_type:
+        if item["job_type"] == job_type:
             jobs_result.append(item)
     return jobs_result
 
@@ -24,8 +30,8 @@ def get_unique_industries(path):
     industries = set()
 
     for item in array_of_jobs:
-        if item['industry']:
-            industries.add(item['industry'])
+        if item["industry"]:
+            industries.add(item["industry"])
     return industries
 
 
@@ -33,7 +39,7 @@ def filter_by_industry(jobs, industry):
     jobs_result = []
 
     for item in jobs:
-        if item['industry'] == industry:
+        if item["industry"] == industry:
             jobs_result.append(item)
     return jobs_result
 
@@ -43,9 +49,9 @@ def get_max_salary(path):
     salaries = []
 
     for item in array_of_jobs:
-        if item['max_salary'].isnumeric():
-            salaries.append(int(item['max_salary']))
-    return (max(salaries))
+        if item["max_salary"].isnumeric():
+            salaries.append(int(item["max_salary"]))
+    return max(salaries)
 
 
 def get_min_salary(path):
@@ -53,31 +59,19 @@ def get_min_salary(path):
     salaries = []
 
     for item in array_of_jobs:
-        if item['min_salary'].isnumeric():
-            salaries.append(int(item['min_salary']))
-    return (min(salaries))
+        if item["min_salary"].isnumeric():
+            salaries.append(int(item["min_salary"]))
+    return min(salaries)
 
 
 def matches_salary_range(job, salary):
-    if not('min_salary' in job.keys()):
-        raise ValueError('min key not contain in jobs')
 
-    if not('max_salary' in job.keys()):
-        raise ValueError('max key not contain in jobs')
+    valid_keys_in_job(job)
+    valid_min_and_max_is_integer(job)
+    valid_min_is_greather_than_max(job)
+    valid_salary_is_an_integer(salary)
 
-    if not(isinstance(job['min_salary'], int)):
-        raise ValueError('min is not a integer')
-
-    if not(isinstance(job['max_salary'], int)):
-        raise ValueError('max is not a integer')
-
-    if job['min_salary'] > job['max_salary']:
-        raise ValueError('min value is greather than max value')
-
-    if type(salary) != int:
-        raise ValueError('salary is not a integer')
-
-    if not(salary in range(int(job['min_salary']), int(job['max_salary']))):
+    if not (salary in range(int(job["min_salary"]), int(job["max_salary"]))):
         return False
 
     return True
---
+++ src/validations.py
@@ -0,0 +1,51 @@
+def valid_keys_in_job(job):
+    if not('min_salary' in job.keys()):
+        raise ValueError('min key not contain in jobs')
+
+    if not('max_salary' in job.keys()):
+        raise ValueError('max key not contain in jobs')
+
+    return True
+
+
+def valid_min_and_max_is_integer(job):
+    if not(isinstance(job['min_salary'], int)):
+        raise ValueError('min is not a integer')
+
+    if not(isinstance(job['max_salary'], int)):
+        raise ValueError('max is not a integer')
+
+    return True
+
+
+def valid_min_is_greather_than_max(job):
+    if job['min_salary'] > job['max_salary']:
+        raise ValueError('min value is greather than max value')
+
+    return True
+
+
+def valid_salary_is_an_integer(salary):
+    if type(salary) != int:
+        raise ValueError('salary is not a integer')
+
+    return True
+
+
+# if not('min_salary' in job.keys()):
+#     raise ValueError('min key not contain in jobs')
+
+# if not('max_salary' in job.keys()):
+#     raise ValueError('max key not contain in jobs')
+
+# if not(isinstance(job['min_salary'], int)):
+#     raise ValueError('min is not a integer')
+
+# if not(isinstance(job['max_salary'], int)):
+#     raise ValueError('max is not a integer')
+
+# if job['min_salary'] > job['max_salary']:
+#     raise ValueError('min value is greather than max value')
+
+# if type(salary) != int:
+#     raise ValueError('salary is not a integer')
