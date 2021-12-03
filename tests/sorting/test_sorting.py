aebd445282cba31f19ed964bcf5081441e98f467
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-12-03 16:51:13 -0300

    test(sort_by): testing sort criteria
    - Criteria parameter must have one of these values: min_salary, max_salary and date_posted
    - Ordering for min_salary must be ascending, but for max_salary or date_posted it must be descending

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,62 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+stub_jobs = [
+    {
+        "job_title": "Senior Salesforce Developer",
+        "min_salary": 44587,
+        "max_salary": 82162,
+        "date_posted": "2020-05-08",
+    },
+    {
+        "job_title": "ABA Therapist",
+        "min_salary": 20000,
+        "max_salary": 35000,
+        "date_posted": "2020-05-07",
+    },
+    {
+        "job_title": "Principal, Sr. Consultant – Creative Technologist",
+        "min_salary": 64829,
+        "max_salary": 104769,
+        "date_posted": "2020-05-05",
+    },
+    {
+        "job_title": "Doctor of Veterinary Medicine",
+        "min_salary": 81991,
+        "max_salary": 120117,
+        "date_posted": "2020-05-02",
+    },
+]
+
+senior_dev, aba_therapist, principal_consultant, doctor_veterinary = stub_jobs
+
+result_min_salary_sort = [
+    aba_therapist,
+    senior_dev,
+    principal_consultant,
+    doctor_veterinary,
+]
+result_max_salary_sort = [
+    doctor_veterinary,
+    principal_consultant,
+    senior_dev,
+    aba_therapist,
+]
+result_date_posted_sort = [
+    senior_dev,
+    aba_therapist,
+    principal_consultant,
+    doctor_veterinary,
+]
 
 
 def test_sort_by_criteria():
-    pass
+    stub_jobs_copy = stub_jobs.copy()
+
+    sort_by(stub_jobs_copy, "min_salary")
+    assert stub_jobs_copy == result_min_salary_sort
+
+    sort_by(stub_jobs_copy, "max_salary")
+    assert stub_jobs_copy == result_max_salary_sort
+
+    sort_by(stub_jobs_copy, "date_posted")
+    assert stub_jobs_copy == result_date_posted_sort
