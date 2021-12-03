bc5c424d3a1bda5d82674503a8ba797ae7d5b3a6
Gabriel <gabriel.mirandab92@gmail.com>
2021-12-03 09:47:30 -0300

    add exercicio 6

--- src/insights.py
+++ src/insights.py
@@ -13,22 +13,13 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
+    filtered_jobs = []
 
-    """Filters a list of jobs by job_type
+    for job in jobs:
+        if job["job_type"] == job_type:
+            filtered_jobs.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return filtered_jobs
 
 
 def get_unique_industries(path):
