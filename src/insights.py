da97c6dfc585c886a66e0637228124b6919e343e
walterlbf <walter.lbf.f@gmail.com>
2021-12-03 14:45:19 -0400

    funcao max salary implementada e passando localmente

--- src/insights.py
+++ src/insights.py
@@ -55,21 +55,15 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    dict_from_jobs = read(path)
+    max_salary = 0
+    for job in dict_from_jobs:
+        if(
+            job["max_salary"].isnumeric()
+            and int(job["max_salary"]) > max_salary
+        ):
+            max_salary = int(job["max_salary"])
+    return max_salary
 
 
 def get_min_salary(path):
