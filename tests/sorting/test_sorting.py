f31387e02538429705a718d26b5afac751bc63a7
denis-rossati <denis.rossatiramos@gmail.com>
2021-12-03 08:03:33 -0300

    adding 'test_sort_by' function

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -42,48 +42,21 @@ job_with_list_values = [
 ]
 
 
-def test_raises_error_with_invalid_criteria():
+def test_sort_by():
     with pytest.raises(ValueError):
         sort_by([], 'invalid_criteira')
-
-
-def test_sort_by_max_salary_criteria():
-    assert sort_by(job_list, 'max_salary') == job_list_ordened_by_max_salary
-
-
-def test_sort_with_max_salary_str():
     with pytest.raises(ValueError):
         sort_by(job_with_invalid_str_values, 'max_salary')
-
-
-def test_sort_with_max_salary_list():
     with pytest.raises(TypeError):
         sort_by(job_with_list_values, 'max_salary')
-
-
-def test_sort_by_min_salary_criteria():
-    assert sort_by(job_list, 'min_salary') == job_list_ordened_by_min_salary
-
-
-def test_sort_with_min_salary_str():
     with pytest.raises(ValueError):
         sort_by(job_with_invalid_str_values, 'min_salary')
-
-
-def test_sort_with_min_salary_list():
     with pytest.raises(TypeError):
         sort_by(job_with_list_values, 'min_salary')
-
-
-def test_sort_by_date_posted_criteria():
-    assert sort_by(job_list, 'date_posted') == job_list_ordened_by_posted_date
-
-
-def test_sort_with_date_posted_str():
     with pytest.raises(NameError):
         sort_by(job_with_invalid_str_values, 'date_posted')
-
-
-def test_sort_with_date_posted_list():
     with pytest.raises(NameError):
         sort_by(job_with_list_values, 'date_posted')
+    assert sort_by(job_list, 'max_salary') == job_list_ordened_by_max_salary
+    assert sort_by(job_list, 'min_salary') == job_list_ordened_by_min_salary
+    assert sort_by(job_list, 'date_posted') == job_list_ordened_by_posted_date
