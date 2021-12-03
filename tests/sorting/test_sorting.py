e04d9342c05a12fb166ccd5fba9fa4396ada43de
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-12-03 15:07:02 -0300

    update: test_sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -2,28 +2,48 @@ from src.sorting import sort_by
 import pytest
 
 jobs_list = [
-    {"min_salary": 2000, "max_salary": 6000, "date_posted": "2020-0101"},
-    {"min_salary": 1000, "max_salary": 5000, "date_posted": "2019-01-01"},
-    {"min_salary": 3000, "max_salary": 7000, "date_posted": "2021-01-01"},
+    {"date_posted": "2020-01-01", "max_salary": 6000, "min_salary": 2000},
+    {"date_posted": "2021-01-01", "max_salary": 5000, "min_salary": 1000},
+    {"date_posted": "2019-01-01", "max_salary": 7000, "min_salary": 3000},
 ]
 
 jobs_list_min = [
-    {"min_salary": 1000, "max_salary": 5000, "date_posted": "2019-01-01"},
-    {"min_salary": 2000, "max_salary": 6000, "date_posted": "2020-01-01"},
-    {"min_salary": 3000, "max_salary": 7000, "date_posted": "2021-01-01"},
+    jobs_list[1],
+    jobs_list[0],
+    jobs_list[2],
 ]
 
-jobs_list_max_and_date = [
-    {"min_salary": 3000, "max_salary": 7000, "date_posted": "2021-01-01"},
-    {"min_salary": 2000, "max_salary": 6000, "date_posted": "2020-01-01"},
-    {"min_salary": 1000, "max_salary": 5000, "date_posted": "2019-01-01"},
+jobs_list_max = [
+    jobs_list[2],
+    jobs_list[0],
+    jobs_list[1],
 ]
+
+jobs_list_date = [
+    jobs_list[1],
+    jobs_list[0],
+    jobs_list[2],
+]
+
+sort_by(
+    [
+        {"date_posted": "2021-01-01", "max_salary": 5000, "min_salary": 1000},
+        {"date_posted": "2020-01-01", "max_salary": 6000, "min_salary": 2000},
+        {"date_posted": "2019-01-01", "max_salary": 7000, "min_salary": 3000},
+    ],
+    "date_posted",
+)
 
 
 def test_sort_by_criteria():
-    assert sort_by(jobs_list, "min_salary") == jobs_list_min
-    assert sort_by(jobs_list, "max_salary") == jobs_list_max_and_date
-    assert sort_by(jobs_list, "date_posted") == jobs_list_max_and_date
+    sort_by(jobs_list, "min_salary")
+    assert jobs_list == jobs_list_min
+
+    sort_by(jobs_list, "max_salary")
+    assert jobs_list == jobs_list_max
+
+    sort_by(jobs_list, "date_posted")
+    assert jobs_list == jobs_list_date
 
     with pytest.raises(
         TypeError, match="missing 1 required positional argument: 'criteria'"
