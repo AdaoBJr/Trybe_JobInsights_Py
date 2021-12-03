0cff7a928a3e9e082400f9b11b14613261c76898
Leandro Reis <lleandrofr@gmail.com>
2021-12-03 10:50:29 -0300

    req10: refactor test for the sort_by function

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,25 +1,31 @@
 from src.sorting import sort_by
 
 salary_list = [
-    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2020-01-01"},
+    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
     {"min_salary": 1000, "max_salary": 4000, "date_posted": "2019-01-01"},
-    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2021-01-01"},
+    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-01-01"},
 ]
 
-min_salary_list = [
+sorted_by_min_salary = [
     {"min_salary": 1000, "max_salary": 4000, "date_posted": "2019-01-01"},
-    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2020-01-01"},
-    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2021-01-01"},
+    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
+    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-01-01"},
 ]
 
-max_salary_list = [
-    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2021-01-01"},
-    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2020-01-01"},
+sorted_by_max_salary_list = [
+    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-01-01"},
+    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
+    {"min_salary": 1000, "max_salary": 4000, "date_posted": "2019-01-01"},
+]
+
+sorted_by_date_posted = [
+    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-01-01"},
+    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
     {"min_salary": 1000, "max_salary": 4000, "date_posted": "2019-01-01"},
 ]
 
 
 def test_sort_by_criteria():
-    assert sort_by(salary_list, "min_salary") == min_salary_list
-    assert sort_by(salary_list, "max_salary") == max_salary_list
-    assert sort_by(salary_list, "date_posted") == max_salary_list
+    assert sort_by(salary_list, "min_salary") == sorted_by_min_salary
+    assert sort_by(salary_list, "max_salary") == sorted_by_max_salary_list
+    assert sort_by(salary_list, "date_posted") == sorted_by_date_posted
