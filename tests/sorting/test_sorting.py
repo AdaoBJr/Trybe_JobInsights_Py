07b3c9c68e14cc282897e8612b0f501a96add417
Rafael <rmathscoach@gmail.com>
2021-12-03 18:29:17 -0300

    requirement 10: done

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,16 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+# Esse teste deve se chamar test_sort_by_criteria e garantir que # a função funciona segundo esta especificação:
+
+# A função sort_by recebe dois parâmetros:
+# jobs uma lista de dicionários com os detalhes de cada emprego;
+# criteria uma string com uma chave para ser usada como critério de ordenação.
+# O parâmetro criteria deve ter um destes
+# valores: min_salary, max_salary, date_posted
 
 
 def test_sort_by_criteria():
-    pass
+    assert sort_by(
+        [{"title_job": "Senior Software Engineer", "company": "Google"}],
+        "min_salary",
+    ) is None
