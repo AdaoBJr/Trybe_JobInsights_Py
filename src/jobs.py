5cd8f3679f57595d22d6197021a882a410defad8
isTorres <ismaeltorres612@gmail.com>
2021-12-03 10:18:39 -0300

    feat: add function to read headres of jobs

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
+    headers = []
+    with open(path, mode="r") as file:
+        header_of_jobs = csv.DictReader(file)
+        for row in header_of_jobs:
+            headers.append(row)
     return []
