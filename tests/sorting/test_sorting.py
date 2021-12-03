aeab4ceaf60293ef95c2ff53a0c553fd156d48b9
Daniel <dsouzaborini@gmail.com>
2021-12-03 17:29:42 -0300

    fix test function

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -26,9 +26,9 @@ def test_sort_by_criteria():
 
     sort_by(jobs, "date_posted")
     assert jobs == [
-        {"min_salary": 9471, "max_salary": 10327, "date_posted": "2020-05-05"},
         {"min_salary": 4458, "max_salary": 8216, "date_posted": "2020-05-08"},
         {"min_salary": 2000, "max_salary": 3500, "date_posted": "2020-05-07"},
+        {"min_salary": 9471, "max_salary": 10327, "date_posted": "2020-05-05"},
     ]
 
     with pytest.raises(ValueError, match="invalid sorting criteria: batata"):
