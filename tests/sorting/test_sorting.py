336f6b49c73521b4b6b1e49d81767b9a76489688
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-12-02 22:45:57 -0300

    testando criteria inexistente

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,15 +1,24 @@
 from src.sorting import sort_by
-
-jobs = [
-    {"max_salary": 0, "min_salary": 10},
-    {"max_salary": 10, "min_salary": 100},
-    {"max_salary": 10000, "min_salary": 200},
-    {"max_salary": 15000, "min_salary": 0},
-    {"max_salary": 1500, "min_salary": 0},
-    {"max_salary": -1, "min_salary": 10},
-]
+import pytest
 
 
-def test_sort_by_criteria():
-    report = sort_by(jobs, "min_salary")
-    assert {"max_salary": 0, "min_salary": 10} in report
+@pytest.fixture
+def stock():
+    return [
+        {"max_salary": 15000, "min_salary": 0},
+        {"max_salary": 10000, "min_salary": 200},
+        {"max_salary": 1500, "min_salary": 0},
+        {"max_salary": 10, "min_salary": 100},
+        {"max_salary": 0, "min_salary": 10},
+        {"max_salary": -1, "min_salary": 10},
+    ]
+
+
+def test_sort_by_criteria(stock):
+    report = sort_by(stock, "min_salary")
+    assert {"max_salary": 15000, "min_salary": 0} in report
+
+
+def test_sort_with_wrong_criteria(stock):
+    report = sort_by(stock, "salary")
+    assert {"max_salary": 15000, "min_salary": 0} not in report
