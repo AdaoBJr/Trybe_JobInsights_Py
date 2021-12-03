6c9b27a46e6971d4fd1fcca2dd7f2a1a0efb33a1
Jefferson <jffrsncrls@gmail.com>
2021-12-02 22:26:45 -0300

    Done: Reque 10

--- src/insights.py
+++ src/insights.py
@@ -10,21 +10,11 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    listaJobs = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            listaJobs.append(job)
+    return listaJobs
 
 
 def get_unique_industries(path):
@@ -37,21 +27,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    listIndustries = []
+    for job in jobs:
+        if job["industry"] == industry:
+            listIndustries.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return listIndustries
 
 
 def get_max_salary(path):
@@ -77,44 +58,28 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    if "max_salary" not in job or "min_salary" not in job:
+        raise ValueError()
+    salary_test = isinstance(salary, int)
+    max_test = isinstance(job["max_salary"], int)
+    min_test = isinstance(job["min_salary"], int)
+    if not salary_test or not max_test or not min_test:
+        raise ValueError()
+    elif job["max_salary"] < job["min_salary"]:
+        raise ValueError()
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if job['min_salary'] <= salary <= job['max_salary']:
+        return True
+    return False
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
+    listaSalario = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                listaSalario.append(job)
+        except ValueError:
+            pass
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    return listaSalario
