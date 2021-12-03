dede7fba6e59aedbf3255487f042eb418e78c3f0
Cristian Bugs <cristian.bugs@gmail.com>
2021-12-03 14:03:46 -0300

    reader

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
+    with open("jobs.csv") as jobs_file:
+        jobs_list = csv.DictReader(jobs_file)
+        print(jobs_list)
     """Reads a file from a given path and returns its contents
 
     Parameters
