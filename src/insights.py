7d75d5bd8e9f5861b944ce0c91e92721a52d3441
dennisMarcelo <dennismarcelo15@gmail.com>
2021-12-02 21:04:17 -0300

    requirement 3

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    data = read(path)
+    all_industries = []
+    for row in data:
+        if row['industry'] not in all_industries and row['industry'] != '':
+            all_industries.append(row['industry'])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return all_industries
 
 
 def filter_by_industry(jobs, industry):
