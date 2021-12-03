f69da1b3bc35c6cca7a8c8fec94e221afe38526b
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-12-03 17:03:58 -0300

    Inserir - tests - Criação do teste da função de ordenar os jobs

--- src/jobs.py
+++ src/jobs.py
@@ -7,6 +7,6 @@ def read(path):
     with open(path) as file:
         csv_file = csv.DictReader(file, delimiter=",", quotechar='"')
         jobs = []
-        for job in list(csv_file):
+        for job in csv_file:
             jobs.append(job)
         return jobs
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,14 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.jobs import read
+from src.insights import get_min_salary
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = read('src/jobs.csv')
+    criteria = "min_salary"
+
+    sort_by(jobs, criteria)
+    min_salary_jobs_sorted = jobs[0][criteria]
+
+    assert type(jobs) is list
+    assert min_salary_jobs_sorted == str(get_min_salary('src/jobs.csv'))
