f76f3a65fccc36fa23deb11ba85759ecd055412f
jaarguello <johnny.arguello007@gmail.com>
2021-12-03 18:52:47 -0300

    requirement 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,14 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    critery = ['min_salary', 'max_salary', 'date_posted']
+    jobs = [{"max_salary": 10000, "min_salary": 200, 'date_posted': '03-10-2019'}, {"max_salary": 1500, "min_salary": 0, 'date_posted': '02-10-2019'}]
+
+    jobs_min_salary = [{"max_salary": 1500, "min_salary": 0, 'date_posted': '02-10-2019'}, {"max_salary": 10000, "min_salary": 200, 'date_posted': '03-10-2019'}]
+    jobs_max_salary = [{"max_salary": 10000, "min_salary": 200, 'date_posted': '03-10-2019'}, {"max_salary": 1500, "min_salary": 0, 'date_posted': '02-10-2019'}]
+    jobs_date_posted = [{"max_salary": 10000, "min_salary": 200, 'date_posted': '03-10-2019'}, {"max_salary": 1500, "min_salary": 0, 'date_posted': '02-10-2019'}]
+
+    assert sort_by(jobs, critery[0]) == jobs_min_salary
+    assert sort_by(jobs, critery[1]) == jobs_max_salary
+    assert sort_by(jobs, critery[2]) == jobs_date_posted
