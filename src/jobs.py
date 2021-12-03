4446b52affadae6e093b115b59314d30910270d5
Gildeir <gildeirlopes@gmail.com>
2021-12-03 00:04:35 -0300

    Feature: reading a file from a given path and returning its contents

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,24 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    # """Reads a file from a given path and returns its contents
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+    # Parameters
+    # ----------
+    # path : str
+    #     Full path to file
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    # Returns
+    # -------
+    # list
+    #     List of rows as dicts
+    # """
+    with open(path) as file:
+        dictionary = []
+        list = csv.DictReader(file, delimiter=',', quotechar='"')
+        for rows in list:
+            dictionary.append(rows)
+        return dictionary
