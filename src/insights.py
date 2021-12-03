5e61d22c3dc8b82c7c8b11b6a86eeda6c7d5a333
João Victor Pistorio <joao.pistorio@gmail.com>
2021-12-03 18:57:21 -0300

    requirement 3

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    industries = set()
+    dict_jobs = read(path)
+    for category in dict_jobs:
+        if category["industry"] != "":
+            industries.add(category["industry"])
+    return list(industries)
 
 
 def filter_by_industry(jobs, industry):
