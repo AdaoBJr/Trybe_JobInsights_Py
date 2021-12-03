2dc4dc343959d12e168d7da4aa27ede33a27dd30
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-12-03 11:06:37 -0300

    fix teste

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,24 +1,31 @@
 from src.sorting import sort_by
+import pytest
+
 
 data = [
-    {"min_salary": 3, "max_salary": 3, "date_posted": "03/01/2000"},
-    {"min_salary": 1, "max_salary": 1, "date_posted": "01/01/2000"},
-    {"min_salary": 2, "max_salary": 2, "date_posted": "02/01/2000"},
+    {"date_posted": "01/01/2000", "max_salary": 1, "min_salary": 1},
+    {"date_posted": "02/01/2000", "max_salary": 2, "min_salary": 2},
+    {"date_posted": "03/01/2000", "max_salary": 3, "min_salary": 3},
 ]
 
-expected_min_salary = [
-    {"min_salary": 1, "max_salary": 1, "date_posted": "01/01/2000"},
-    {"min_salary": 2, "max_salary": 2, "date_posted": "01/01/2000"},
-    {"min_salary": 3, "max_salary": 3, "date_posted": "01/01/2000"},
-]
 
-expected_max_salary = [
-    {"min_salary": 3, "max_salary": 3, "date_posted": "03/01/2000"},
-    {"min_salary": 2, "max_salary": 2, "date_posted": "02/01/2000"},
-    {"min_salary": 1, "max_salary": 1, "date_posted": "01/01/2000"},
-]
+# expected
+expected_min_salary = [data[0], data[1], data[2]]
+
+expected_max_salary = [data[2], data[1], data[0]]
 
 
 def test_sort_by_criteria():
-    report = sort_by(data, "max_salary")
-    assert expected_max_salary in report
+    sort_by(data, "min_salary")
+    assert expected_min_salary == data
+
+    sort_by(data, "max_salary")
+    assert expected_max_salary == data
+
+    sort_by(data, "date_posted")
+    assert expected_max_salary == data
+
+    with pytest.raises(
+        TypeError, match="missing 1 required positional argument: 'criteria'"
+    ):
+        sort_by(data)
