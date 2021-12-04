60b521b8acba44a1dc74f4ddcccfd62d81a8d19a
Lucas Ribeiro <lucasrb04@gmail.com>
2021-12-04 11:36:31 -0300

    Req.1 refatorando

--- src/jobs.py
+++ src/jobs.py
@@ -5,12 +5,4 @@ import csv
 @lru_cache
 def read(path):
     with open(path) as file:
-        job_description = csv.reader(file, delimiter=",", quotechar='"')
-        header, *data = job_description
-        jobs = []
-        for job in data:
-            job_description = {}
-            for idx, field in enumerate(header):
-                job_description[field] = job[idx]
-            jobs.append(job_description)
-    return jobs
+        return list(csv.DictReader(file))
