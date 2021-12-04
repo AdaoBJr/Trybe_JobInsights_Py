4b618b30cd2d49bbb1ff411404bbb1015b89fe40
Camila Arruda <camilaam1802@gmail.com>
2021-12-04 09:50:40 -0300

    requisito 1 teste

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,17 @@
 from functools import lru_cache
 
+import csv
+
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
+    """https://acervolima.com/usando-o-modulo-csv-para-ler-os-dados-no-pandas/
     """
-    return []
+    with open(path) as file_csv:
+        return list(csv.DictReader(file_csv))
+        """vai abrir o arquivo do path
+        e retorna a lista de dicionarios
+        """
+
+
+read("src/jobs.csv")
