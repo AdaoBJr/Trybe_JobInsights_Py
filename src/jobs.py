e852ddb3bd541fd146edd64fc23f297892723a4e
hugolfsimoes <hugolfsimoes@gmail.com>
2021-12-04 01:15:23 -0300

    feito o requisito 2

--- src/jobs.py
+++ src/jobs.py
@@ -5,7 +5,7 @@ import csv
 @lru_cache
 def read(path):
     with open(path, "r") as jobs_file:
-        jobs_csv = csv.DictReader(jobs_file)
+        jobs_csv = csv.DictReader(jobs_file, delimiter=",")
         lines = []
         for line in jobs_csv:
             lines.append(line)
