1d84c66c3ddbd51c5806bcc3652195c818067108
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-12-04 00:33:59 -0300

    9 finished

--- src/insights.py
+++ src/insights.py
@@ -103,21 +103,14 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    inSalaryRange = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                inSalaryRange.append(job)
+        except ValueError:
+            pass
+    return inSalaryRange
 
 
 if __name__ == "__main__":
