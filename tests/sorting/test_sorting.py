af5231ad3b28779b91269d0ff73f3772eed48338
André Arnoni <andre_arnoni@yahoo.com.br>
2021-12-03 21:25:20 -0300

    executado requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,14 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.insights import get_max_salary
+from src.jobs import read
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = read("src/jobs.csv")
+    criteria = "max_salary"
+
+    sort_by(jobs, criteria)
+    sort_jobs = jobs[0][criteria]
+
+    assert type(jobs) is list
+    assert sort_jobs == str(get_max_salary("src/jobs.csv"))
