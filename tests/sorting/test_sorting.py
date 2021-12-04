c27ba9392023f341889cd2cf1b2f91bdf332e3d0
Lara-Capila <laracapilak@gmail.com>
2021-12-04 16:14:44 -0300

    feat(req10): Requisito 10 ainda sem passar em um sub test(min_salary)

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,27 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+jobs = [
+    {"min_salary": 700, "max_salary": 3000, "date_posted": "2020-05-10", },
+    {"min_salary": 600, "max_salary": 2000, "date_posted": "2020-05-09", },
+    {"min_salary": 500, "max_salary": 1000, "date_posted": "2020-05-08", },
+]
+
+min_salary = [
+    {"min_salary": 1000, "max_salary": 4000, "date_posted": "2020-05-10", },
+    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2020-05-09", },
+    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-05-08", },
+]
+
+max_salary = [
+    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-05-10", },
+    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2020-05-09", },
+    {"min_salary": 1000, "max_salary": 4000, "date_posted": "2020-05-08", },
+]
+
+# da pra fazer isso com sort????
 
 
 def test_sort_by_criteria():
-    pass
+    assert sort_by(jobs, "min_salary") == min_salary
+    assert sort_by(jobs, "max_salary") == max_salary
+    assert sort_by(jobs, "date_posted") == jobs
