da4790799e3498818fcca31bc825c994ff5a8669
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-12-04 18:53:05 -0300

    req3

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,14 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    list_jobs = read(path)
+    jobs_industry = set()
+    for job in list_jobs:
+        jobs_industry.add(job["industry"])
+    return jobs_industry
 
-    Must call `read`
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+get_unique_industries("src/jobs.csv")
 
 
 def filter_by_industry(jobs, industry):
