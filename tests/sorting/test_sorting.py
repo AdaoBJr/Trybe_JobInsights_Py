fd9791e1b2f72e6c6224445af3f3c7906387a200
EderPaiva <edergomesdepaiva12@gmail.com>
2021-12-05 16:43:49 -0300

    lançando o erro

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,24 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+import pytest
+
+CRITERIA = ['min_salary', 'max_salary', 'date_posted']
+
+JOBS = [
+    {"job_title": "Dev Front",
+        "min_salary": 700,
+        "max_salary": 3000,
+        "date_posted": "2020-05-10", },
+    {"job_title": "Dev Back",
+        "min_salary": 600,
+        "max_salary": 2000,
+        "date_posted": "2020-05-09", },
+    {"job_title": "DevOps",
+        "min_salary": 500,
+        "max_salary": 1000,
+        "date_posted": "2020-05-08", },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    with pytest.raises(ValueError, match="invalid sorting criteria: tanajura"):
+        sort_by(JOBS, 'tanajura')
