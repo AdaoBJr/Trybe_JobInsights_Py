510930345a129acbf6aa83643db7e9dd8d741ee3
joao966 <joaooliveira966@gmail.com>
2021-12-05 16:26:51 -0300

    ADD: request-8 anterior era 7

--- src/insights.py
+++ src/insights.py
@@ -162,7 +162,19 @@ def matches_salary_range(job, salary):
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    pass
+    if (
+        "min_salary" not in job
+        or "max_salary" not in job
+        or type(job["min_salary"]) != int
+        or type(job["max_salary"]) != int
+        or job["min_salary"] > job["max_salary"]
+        or type(salary) != int
+    ):
+        raise ValueError()
+    elif salary >= job["min_salary"] and salary <= job["max_salary"]:
+        return True
+    else:
+        return False
 
 
 def filter_by_salary_range(jobs, salary):
