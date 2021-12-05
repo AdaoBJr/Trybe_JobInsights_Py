a61d7cc9df435d99546d405851c63745e0c298f0
Aremtior <marcosleandrofl@gmail.com>
2021-12-05 17:49:43 -0300

    req 2 - list ofjob_types && req 3 - list of industries

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from .jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,13 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    all_jobs = read(path)
+    job_types = set()
+    for job in all_jobs:
+        job_types.add(job["job_type"])
+    return [
+        *job_types,
+    ]  # https://www.geeksforgeeks.org/python-convert-set-into-a-list/
 
 
 def filter_by_job_type(jobs, job_type):
