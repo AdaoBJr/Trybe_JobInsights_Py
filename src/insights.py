d395a5fd95ee60e7e443b99413cd3efcebf49c26
joao966 <joaooliveira966@gmail.com>
2021-12-05 16:35:38 -0300

    ADD: request-9

--- src/insights.py
+++ src/insights.py
@@ -192,4 +192,11 @@ def filter_by_salary_range(jobs, salary):
     list
         Jobs whose salary range contains `salary`
     """
-    return []
+    resultList = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                resultList.append(job)
+        except ValueError:
+            print("Error")
+    return resultList
