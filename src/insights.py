d77bc8a5e19183541300119c882e30e3c2699655
AdaoBJr <adaobjr@gmail.com>
2021-12-05 18:45:53 -0400

    feat: 🔥 removed code not used

--- src/insights.py
+++ src/insights.py
@@ -57,29 +57,7 @@ def filter_by_industry(jobs, industry):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    
 
 
 def filter_by_salary_range(jobs, salary):
