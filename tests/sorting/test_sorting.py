60c20149f9dbe9f63f2f8a74146d66f83dfaec6a
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-12-05 14:55:55 -0300

    req10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,6 @@
 from src.sorting import sort_by
 
+
 salary_list = [
     {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
     {"min_salary": 1000, "max_salary": 4000, "date_posted": "2019-01-01"},
@@ -19,8 +20,8 @@ sorted_by_max_salary_list = [
 ]
 
 sorted_by_date_posted = [
-    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
     {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-01-01"},
+    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
     {"min_salary": 1000, "max_salary": 4000, "date_posted": "2019-01-01"},
 ]
 
