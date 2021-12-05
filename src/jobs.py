62c96fd371ee3e2b14d3b1e5c07ce0071b8d534e
CIGR2021 <appencigr@gmail.com>
2021-12-05 12:11:25 -0300

    feature complete

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,9 @@
 from functools import lru_cache
 import csv
 
+
 @lru_cache
 def read(path):
     with open(path) as jobs:
         jobs_list = csv.DictReader(jobs)
-    return list(jobs_list)
+        return list(jobs_list)
