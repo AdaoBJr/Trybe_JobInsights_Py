795ddcee458e4e8d642ba01a9bc97a5a193802c1
Breno Rodrigues <brenorm@gmail.com>
2021-12-05 21:49:52 -0300

    Requisito 1: Quebra das linhas de comentário por exigência do LINT.

--- src/jobs.py
+++ src/jobs.py
@@ -16,7 +16,11 @@ def read(path):
     list
         List of rows as dicts
     """
-    # Source: https://app.betrybe.com/course/computer-science/introducao-a-python/entrada-e-saida-de-dados/105dc022-72fa-425f-a452-29b3595bb64d/conteudos/9a69f5d2-dd9d-4831-bea0-bb2f7251cc3b/manipulando-arquivos-csv/acbfc282-4ea3-4391-aa85-77f9784efdd2?use_case=side_bar
+    # Source: https://app.betrybe.com/course/computer-science
+    # /introducao-a-python/entrada-e-saida-de-dados/105dc022-
+    # 72fa-425f-a452-29b3595bb64d/conteudos/9a69f5d2-dd9d-483
+    # 1-bea0-bb2f7251cc3b/manipulando-arquivos-csv/acbfc282-4
+    # ea3-4391-aa85-77f9784efdd2?use_case=side_bar
     with open(path) as file:
         content = csv.reader(file, delimiter=",", quotechar='"')
         header, *data = content
@@ -24,8 +28,17 @@ def read(path):
     jobsList = []
     for row in data:
         jobsDict = {}
-        # Source: https://www.educative.io/edpresso/how-to-get-the-length-of-a-list-in-python?utm_term=&utm_campaign=%5BTest%5D+Dynamic+Verticals&utm_source=adwords&utm_medium=ppc&hsa_acc=5451446008&hsa_cam=14045073269&hsa_grp=128822123241&hsa_ad=535845844735&hsa_src=g&hsa_tgt=dsa-1394252596758&hsa_kw=&hsa_mt=&hsa_net=adwords&hsa_ver=3
-        # Source: https://app.betrybe.com/course/computer-science/introducao-a-python/aprendendo-python/9c4e1d64-303d-492d-82a4-998b2c0218b9/conteudos/217980af-0946-4b04-9cc4-5846af26ec27/tipos-de-dados-embutidos/bea4b2ef-aece-43fe-9d9d-fa7d8c183088?use_case=side_bar
+        # Source: https://www.educative.io/edpresso/how-to-get-th
+        # e-length-of-a-list-in-python?utm_term=&utm_campaign=%5B
+        # Test%5D+Dynamic+Verticals&utm_source=adwords&utm_medium
+        # =ppc&hsa_acc=5451446008&hsa_cam=14045073269&hsa_grp=128
+        # 822123241&hsa_ad=535845844735&hsa_src=g&hsa_tgt=dsa-139
+        # 4252596758&hsa_kw=&hsa_mt=&hsa_net=adwords&hsa_ver=3
+        # Source: https://app.betrybe.com/course/computer-science
+        # /introducao-a-python/aprendendo-python/9c4e1d64-303d-49
+        # 2d-82a4-998b2c0218b9/conteudos/217980af-0946-4b04-9cc4-
+        # 5846af26ec27/tipos-de-dados-embutidos/bea4b2ef-aece-43f
+        # e-9d9d-fa7d8c183088?use_case=side_bar
         for index in range(0, len(row)):
             jobsDict[header[index]] = row[index]
         jobsList.append(jobsDict)
