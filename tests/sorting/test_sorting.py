fbd5d2955116d7ce9be380f8f5e190ad9cb3dc9c
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-12-06 18:17:22 -0300

    Req10 testando funçao de ordenacao

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,28 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+import pytest
 
 
 def test_sort_by_criteria():
-    pass
+    data = [
+        {"date_posted": "06/06/2021", "max_salary": 14, "min_salary": 12},
+        {"date_posted": "07/06/2021", "max_salary": 15, "min_salary": 11},
+        {"date_posted": "08/06/2021", "max_salary": 16, "min_salary": 10},
+    ]
+
+    data_sort_max = [data[2], data[1], data[0]]
+    data_sort_min = [data[2], data[1], data[0]]
+    data_sort_posted = [data[2], data[1], data[0]]
+
+    sort_by(data, "max_salary")
+    assert data == data_sort_max
+
+    sort_by(data, "min_salary")
+    assert data == data_sort_min
+
+    sort_by(data, "date_posted")
+    assert data == data_sort_posted
+
+    with pytest.raises(
+        TypeError, match="criteria"
+    ):
+        sort_by(data)
