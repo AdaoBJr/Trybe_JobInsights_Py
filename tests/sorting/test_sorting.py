de9e99f81cfd7e6e76fd5029e2582471dacd8cbf
Renan0211 <renan.antunes.rocha@gmail.com>
2021-12-06 18:27:43 -0300

    continuando os testes do sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -2,6 +2,80 @@ import pytest
 from src.sorting import sort_by
 from pytest import fixture
 
+jobs = [
+    {
+        "min_salary": 10,
+        "max_salary": 50,
+        "date_posted": "2020-10-01T00:00:00.000Z",
+    },
+    {
+        "min_salary": 20,
+        "max_salary": 60,
+        "date_posted": "2019-10-01T00:00:00.000Z",
+    },
+    {
+        "min_salary": 30,
+        "max_salary": 70,
+        "date_posted": "2018-10-01T00:00:00.000Z",
+    },
+]
+
+valid_criterias = ["min_salary", "max_salary", "date_posted"]
+
+expected_results = [
+    [
+        {
+            "min_salary": 10,
+            "max_salary": 50,
+            "date_posted": "2020-10-01T00:00:00.000Z",
+        },
+        {
+            "min_salary": 20,
+            "max_salary": 60,
+            "date_posted": "2019-10-01T00:00:00.000Z",
+        },
+        {
+            "min_salary": 30,
+            "max_salary": 70,
+            "date_posted": "2018-10-01T00:00:00.000Z",
+        },
+    ],
+    [
+        {
+            "min_salary": 30,
+            "max_salary": 70,
+            "date_posted": "2018-10-01T00:00:00.000Z",
+        },
+        {
+            "min_salary": 20,
+            "max_salary": 60,
+            "date_posted": "2019-10-01T00:00:00.000Z",
+        },
+        {
+            "min_salary": 10,
+            "max_salary": 50,
+            "date_posted": "2020-10-01T00:00:00.000Z",
+        },
+    ],
+    [
+        {
+            "min_salary": 30,
+            "max_salary": 70,
+            "date_posted": "2018-10-01T00:00:00.000Z",
+        },
+        {
+            "min_salary": 20,
+            "max_salary": 60,
+            "date_posted": "2019-10-01T00:00:00.000Z",
+        },
+        {
+            "min_salary": 10,
+            "max_salary": 50,
+            "date_posted": "2020-10-01T00:00:00.000Z",
+        },
+    ],
+]
+
 
 @fixture
 def test_sort_by_criteria():
@@ -9,4 +83,6 @@ def test_sort_by_criteria():
         sort_by([], "not a valid criteria")
         sort_by("not a valid job", "min_salary")
         sort_by([{"still": "not", "a": "valid", "job": ""}], "max_salary")
-    pass
+    for criteria_index in range(len(valid_criterias)):
+        criteria = valid_criterias[criteria_index]
+        assert sort_by(jobs, criteria) == expected_results[criteria_index]
