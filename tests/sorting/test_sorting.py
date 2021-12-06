a66be936a535e0661da6f9f233af185082bf12d3
Fernando <fhomaia@gmail.com>
2021-12-05 23:26:48 -0300

    fix syntax error

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,4 +1,3 @@
-import pytest
 from src.sorting import sort_by
 
 
@@ -23,17 +22,17 @@ def test_sort_by_criteria():
     assert sort_by(jobs, "min_salary") == [
         {"max_salary": 1500, "min_salary": 0, "date_posted": "09-12-2021"},
         {"max_salary": 15000, "min_salary": 10, "date_posted": "10-12-2021"},
-        {"max_salary": 100, "min_salary": 15}
+        {"max_salary": 100, "min_salary": 15},
         {"min_salary": 70, "date_posted": "05-12-2021"},
         {"max_salary": 10000, "min_salary": 200, "date_posted": "11-12-2021"},
         {"max_salary": 130, "date_posted": "14-12-2021"},
     ]
 
     assert sort_by(jobs, "date_posted") == [
-        {"max_salary": 130, "date_posted": "14-12-2021"}
+        {"max_salary": 130, "date_posted": "14-12-2021"},
         {"max_salary": 1500, "min_salary": 0, "date_posted": "11-12-2021"},
         {"max_salary": 15000, "min_salary": 10, "date_posted": "10-12-2021"},
         {"max_salary": 10000, "min_salary": 200, "date_posted": "09-12-2021"},
         {"min_salary": 10, "date_posted": "05-12-2021"},
-        {"max_salary": 100, "min_salary": 15}
+        {"max_salary": 100, "min_salary": 15},
     ]
