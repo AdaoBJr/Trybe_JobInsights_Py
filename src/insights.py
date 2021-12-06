f44f0d8efc77c9a61a84f7d8687f508304640ab2
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-12-06 10:28:13 -0300

    Refatorando Req08 , ajustando de acordo com flake8

--- src/insights.py
+++ src/insights.py
@@ -5,7 +5,7 @@ def get_unique_job_types(path):
     new_data = src.jobs.read(path)
     list_unique_job_types = set()
     for new_job in new_data:
-        list_unique_job_types.add(new_job['job_type'])
+        list_unique_job_types.add(new_job["job_type"])
     return list_unique_job_types
 
     # """Checks all different job types and returns a list of them
@@ -25,12 +25,12 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    list_jobs = []  
+    list_jobs = []
     for new_job in jobs:
-        if new_job['job_type'] == job_type:
+        if new_job["job_type"] == job_type:
             list_jobs.append(new_job)
     return list_jobs
-    
+
     """Filters a list of jobs by job_type
 
     Parameters
@@ -52,8 +52,8 @@ def get_unique_industries(path):
     new_data = src.jobs.read(path)
     list_unique_industries = set()
     for industry in new_data:
-        if industry['industry'] != "":
-            list_unique_industries.add(industry['industry'])
+        if industry["industry"] != "":
+            list_unique_industries.add(industry["industry"])
     return list_unique_industries
     """Checks all different industries and returns a list of them
 
@@ -73,12 +73,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    list_industry = []  
+    list_industry = []
     for job_industry in jobs:
-        if job_industry['industry'] == industry:
+        if job_industry["industry"] == industry:
             list_industry.append(job_industry)
     return list_industry
-    
+
     """Filters a list of jobs by industry
 
     Parameters
@@ -100,8 +100,8 @@ def get_max_salary(path):
     new_data = src.jobs.read(path)
     list_max_salary = set()
     for salary in new_data:
-        if salary['max_salary'].isnumeric():
-            list_max_salary.add(int(salary['max_salary']))
+        if salary["max_salary"].isnumeric():
+            list_max_salary.add(int(salary["max_salary"]))
     return max(list_max_salary)
     """Get the maximum salary of all jobs
 
@@ -124,8 +124,8 @@ def get_min_salary(path):
     new_data = src.jobs.read(path)
     list_min_salary = set()
     for salary in new_data:
-        if salary['min_salary'].isnumeric():
-            list_min_salary.add(int(salary['min_salary']))
+        if salary["min_salary"].isnumeric():
+            list_min_salary.add(int(salary["min_salary"]))
     return min(list_min_salary)
     """Get the minimum salary of all jobs
 
@@ -145,25 +145,21 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    if 'min_salary' not in job or 'max_salary' not in job:
-        raise ValueError('Salario Vazio')
-    
-    if not type(job["min_salary"]) == int:
-        raise ValueError("Sario minimo não numerico")
-        
-    if not type(job["max_salary"]) == int:
-        raise ValueError("Salario maximo não numerico")
-
-    if not type(salary) == int:
-        raise ValueError("Salario informado não numerico")
-        
-    elif job["min_salary"] > job["max_salary"]:
-        raise ValueError("Salario Min maior q Max")
+    if (
+        "min_salary" not in job 
+        or "max_salary" not in job
+        or type(job["min_salary"]) != int
+        or type(job["max_salary"]) != int
+        or type(salary) != int
+        or job["min_salary"] > job["max_salary"]
+    ):
+        raise ValueError("Algo deu erro")
 
     elif job["min_salary"] <= salary <= job["max_salary"]:
         return True
-    return False
-    
+    else:
+        return False
+
     """Checks if a given salary is in the salary range of a given job
 
     Parameters
