f8d8acc310d956bec916bf60877d3503743b96dd
Renan0211 <renan.antunes.rocha@gmail.com>
2021-12-06 18:01:31 -0300

    começando os testes pra funçao sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,12 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
+from pytest import fixture
 
 
+@fixture
 def test_sort_by_criteria():
+    with pytest.raises(ValueError):
+        sort_by([], "not a valid criteria")
+        sort_by("not a valid job", "min_salary")
+        sort_by([{"still": "not", "a": "valid", "job": ""}], "max_salary")
     pass
