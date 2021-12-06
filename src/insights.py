d560090e5066d69f04b67a445be25a59f557e57c
Mariana Mohr <marianamohr@gmail.com>
2021-12-06 16:59:54 -0300

    Req 8 - feito

--- src/insights.py
+++ src/insights.py
@@ -85,29 +85,22 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if (
+        # testa se Jobs tem a chave "min_salary"
+        "min_salary" not in job
+        # testa se Jobs tem a chave "max_salary"
+        or "max_salary" not in job
+        # testa se max_salary e min_salary e salary saão inteiros
+        or type(job["min_salary"]) != int
+        or type(job["max_salary"]) != int
+        or type(salary) != int
+        # testa se min_salary é menor que max_salary
+        or job["min_salary"] > job["max_salary"]
+    ):
+        # se algum dos tests acima der true, aparece o erro
+        raise ValueError
+        # retorna true pois o salario está dentro da faixa
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
