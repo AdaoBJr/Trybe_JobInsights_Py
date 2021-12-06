85bf6f441ee78361ebb2fd115c0b34e01ef3b4c6
Mariana Mohr <marianamohr@gmail.com>
2021-12-06 15:44:49 -0300

    Req 1 - feito

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
 from functools import lru_cache
+import csv
 
 
+# https://docs.python.org/pt-br/3/library/csv.html
+# https://app.betrybe.com/course/computer-science/introducao-a-python/entrada-e-saida-de-dados/105dc022-72fa-425f-a452-29b3595bb64d/conteudos/9a69f5d2-dd9d-4831-bea0-bb2f7251cc3b/manipulando-arquivos-csv/acbfc282-4ea3-4391-aa85-77f9784efdd2?use_case=side_bar
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path) as file:
+        dados = csv.DictReader(file)
+        header, *data = dados
+        return [header, *data]
