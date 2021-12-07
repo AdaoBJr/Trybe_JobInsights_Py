38c36ec192f5849023b1de799581792c6d0ac54d
Carlos <cemargato@gmail.com>
2021-12-07 14:03:03 -0300

    acertos-de-variaveis

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,24 +1,26 @@
 from src.sorting import sort_by
 import pytest
 
+# recebi a ajuda do Eder Paixa e Lara Capila para a resolução dos requisito 8, 9 e 10
+
 jobs = [
     {"min_salary": 2500, "max_salary": 3500, "date_posted": "2021-12-01"},
     {"min_salary": 2000, "max_salary": 3000, "date_posted": "2021-11-01"},
     {"min_salary": 1500, "max_salary": 2500, "date_posted": "2021-10-01"},
 ]
 
-order_by_min = [jobs[2], jobs[1], jobs[0]]
-order_by_max = [jobs[0], jobs[1], jobs[2]]
-order_by_date = [jobs[0], jobs[1], jobs[2]]
+min_salary_ordered = [jobs[2], jobs[1], jobs[0]]
+max_salary_ordered = [jobs[0], jobs[1], jobs[2]]
+date_ordered = [jobs[0], jobs[1], jobs[2]]
 
 
 def test_sort_by_criteria():
     sort_by(jobs, "min_salary")
-    assert jobs == order_by_min
+    assert jobs == min_salary_ordered
     sort_by(jobs, "max_salary")
-    assert jobs == order_by_max
+    assert jobs == max_salary_ordered
     sort_by(jobs, "date_posted")
-    assert jobs == order_by_date
+    assert jobs == date_ordered
 
     with pytest.raises(ValueError, match='invalid sorting'):
         sort_by(jobs, 'premium')
