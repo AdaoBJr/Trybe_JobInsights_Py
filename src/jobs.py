2aa34e6f40c92778e40cd71093749391e4808bc8
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-12-07 14:39:55 -0300

    Feat: create function that return a list of dicts

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,13 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    lista = []
+    with open(path) as file:
+        data = csv.DictReader(file)
+        for d in data:
+            lista.append(d)
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    return lista
