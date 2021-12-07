5efc932e9cb6a4a34da0a7fb56264757a68f4913
antonioarieiro <sfgfffs@gmail.com>
2021-12-07 14:01:50 -0300

    error lint fix

--- src/jobs.py
+++ src/jobs.py
@@ -3,20 +3,7 @@ import csv
 
 @lru_cache
 
-
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
     result = []
     with open(path) as file:
         content = csv.DictReader(file, delimiter=",", quotechar='"')
