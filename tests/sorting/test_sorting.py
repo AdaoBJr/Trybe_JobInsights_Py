c418e41fed3cff89ec998aad29c674d33265cf34
legalvao1 <legalvao1@gmail.com>
2021-12-07 16:27:23 -0300

    Requisito 10 - testando o parametro criteria da funçao sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,17 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
+from src.jobs import read
+
+criteria_keys = [
+    "date_posted",
+    "max_salary",
+    "min_salary",
+]
+
+jobs = read('src/jobs.csv')
 
 
 def test_sort_by_criteria():
-    pass
+    with pytest.raises(ValueError,
+                       match="invalid sorting criteria: date_create"):
+        sort_by(jobs, 'date_create')
