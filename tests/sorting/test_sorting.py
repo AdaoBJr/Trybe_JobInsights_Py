dee858205a802a34e24f8877a8883254ce80b8ed
Jefferson <jffrsncrls@gmail.com>
2021-12-08 12:19:27 -0300

    reque 10: done

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,43 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+jobs = [
+    {"date_posted": "2021-05-01", "max_salary": 0, "min_salary": 500},
+    {"date_posted": "2021-05-02", "max_salary": 500, "min_salary": 0},
+    {"date_posted": "2021-05-03", "max_salary": 7000, "min_salary": 2000},
+    {"date_posted": "2021-05-04", "max_salary": 1000, "min_salary": 2500},
+    {"date_posted": "2021-05-05", "max_salary": 5, "min_salary": 10},
+]
+maxSalaryLista = [
+    jobs[2],
+    jobs[3],
+    jobs[1],
+    jobs[4],
+    jobs[0],
+]
+minSalaryLista = [
+    jobs[1],
+    jobs[4],
+    jobs[0],
+    jobs[2],
+    jobs[3],
+]
+
+dateLista = [
+    jobs[4],
+    jobs[3],
+    jobs[2],
+    jobs[1],
+    jobs[0],
+]
 
 
 def test_sort_by_criteria():
-    pass
+
+    sort_by(jobs, "max_salary")
+    assert jobs == maxSalaryLista
+
+    sort_by(jobs, "min_salary")
+    assert jobs == minSalaryLista
+
+    sort_by(jobs, "date_posted")
+    assert jobs == dateLista
