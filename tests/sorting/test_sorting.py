779b3454c7f8ca80faa7cc217cd9236d0ab87339
legalvao1 <legalvao1@gmail.com>
2021-12-08 00:09:13 -0300

    Requisito 10 - testes da funçao sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,24 +1,50 @@
 import pytest
 from src.sorting import sort_by
-from src.jobs import read
+
 
 invalid_keys = [
     "date_create",
     None,
 ]
 
-valid_keys = [
-        "date_posted",
-        "max_salary",
-        "min_salary"
+# Source https://github.com/tryber/sd-010-b-project-job-insights/pull/64/files
+jobs = [
+        {"max_salary": 94648, "min_salary": 82243,
+                              "date_posted": '2020-04-30'},
+        {"max_salary": 107487, "min_salary": 62576,
+                               "date_posted": '2020-05-05'},
+        {"max_salary": 118794, "min_salary": 81591,
+                               "date_posted": '2020-04-28'},
+    ]
+
+expected_min_salary = [
+    {"max_salary": 107487, "min_salary": 62576, "date_posted": '2020-05-05'},
+    {"max_salary": 118794, "min_salary": 81591, "date_posted": '2020-04-28'},
+    {"max_salary": 94648, "min_salary": 82243, "date_posted": '2020-04-30'},
 ]
 
-jobs = read('src/jobs.csv')
+expected_max_salary = [
+    {"max_salary": 118794, "min_salary": 81591, "date_posted": '2020-04-28'},
+    {"max_salary": 107487, "min_salary": 62576, "date_posted": '2020-05-05'},
+    {"max_salary": 94648, "min_salary": 82243, "date_posted": '2020-04-30'},
+]
+
+expected_date_post = [
+    {"max_salary": 107487, "min_salary": 62576, "date_posted": '2020-05-05'},
+    {"max_salary": 94648, "min_salary": 82243, "date_posted": '2020-04-30'},
+    {"max_salary": 118794, "min_salary": 81591, "date_posted": '2020-04-28'},
+]
 
 
 def test_sort_by_criteria():
-    # result = sort_by(jobs, 'min_salary')
-    # assert result != jobs
+    sort_by(jobs, 'min_salary')
+    assert jobs == expected_min_salary
+
+    sort_by(jobs, 'max_salary')
+    assert jobs == expected_max_salary
+
+    sort_by(jobs, 'date_posted')
+    assert jobs == expected_date_post
 
     for criteria in invalid_keys:
         with pytest.raises(ValueError,
