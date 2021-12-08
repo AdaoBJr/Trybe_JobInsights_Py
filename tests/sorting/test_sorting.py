df7f56d6ed36e02ac001715cc94588822c498db5
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-12-08 20:22:33 -0300

    project delivery done

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,82 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+jobs = [
+    {
+        "min_salary": 200,
+        "max_salary": 500,
+        "date_posted": "2021-12-01",
+    },
+    {
+        "min_salary": 100,
+        "max_salary": 300,
+        "date_posted": "2021-12-10",
+    },
+    {
+        "min_salary": 700,
+        "max_salary": 1000,
+        "date_posted": "2021-12-03",
+    },
+]
+
+ascending = [
+    {
+        "min_salary": 100,
+        "max_salary": 300,
+        "date_posted": "2021-12-10",
+    },
+    {
+        "min_salary": 200,
+        "max_salary": 500,
+        "date_posted": "2021-12-01",
+    },
+    {
+        "min_salary": 700,
+        "max_salary": 1000,
+        "date_posted": "2021-12-03",
+    },
+]
+
+descending = [
+    {
+        "min_salary": 700,
+        "max_salary": 1000,
+        "date_posted": "2021-12-03",
+    },
+    {
+        "min_salary": 200,
+        "max_salary": 500,
+        "date_posted": "2021-12-01",
+    },
+    {
+        "min_salary": 100,
+        "max_salary": 300,
+        "date_posted": "2021-12-10",
+    },
+]
+
+dateposted = [
+    {
+        "min_salary": 100,
+        "max_salary": 300,
+        "date_posted": "2021-12-10",
+    },
+    {
+        "min_salary": 700,
+        "max_salary": 1000,
+        "date_posted": "2021-12-03",
+    },
+    {
+        "min_salary": 200,
+        "max_salary": 500,
+        "date_posted": "2021-12-01",
+    },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, "min_salary")
+    assert jobs == descending
+    sort_by(jobs, "max_salary")
+    assert jobs == ascending
+    sort_by(jobs, "date_posted")
+    assert jobs == dateposted
