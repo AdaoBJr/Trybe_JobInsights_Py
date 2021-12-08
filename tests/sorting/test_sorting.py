0007925291c975d621abac0049f611a5f9d6b170
legalvao1 <legalvao1@gmail.com>
2021-12-07 21:46:34 -0300

    Testando se o avaliador roda

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -2,16 +2,25 @@ import pytest
 from src.sorting import sort_by
 from src.jobs import read
 
-criteria_keys = [
-    "date_posted",
-    "max_salary",
-    "min_salary",
+invalid_keys = [
+    "date_create",
+    None,
+]
+
+valid_keys = [
+        "date_posted",
+        "max_salary",
+        "min_salary"
 ]
 
 jobs = read('src/jobs.csv')
 
 
 def test_sort_by_criteria():
-    with pytest.raises(ValueError,
-                       match="invalid sorting criteria: date_create"):
-        sort_by(jobs, 'date_create')
+    # result = sort_by(jobs, 'min_salary')
+    # assert result != jobs
+
+    for criteria in invalid_keys:
+        with pytest.raises(ValueError,
+                           match=f"invalid sorting criteria: {criteria}"):
+            sort_by(jobs, criteria)
