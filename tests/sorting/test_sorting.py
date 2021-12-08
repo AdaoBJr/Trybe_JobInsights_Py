b1d68b8ef3ecedf928192792dbc205eb134b8bb9
Gildeir <gildeirlopes@gmail.com>
2021-12-07 22:19:11 -0300

    test_sort_by_criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,31 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+list = [
+    {'min_salary': 1500, 'max_salary': 22000, 'date_posted': '2020-04-19'},
+    {'min_salary': 2300, 'max_salary': 25000, 'date_posted': '2019-01-19'},
+    {'min_salary': 3000, 'max_salary': 50000, 'date_posted': '2021-01-21'},
+]
+
+min_salary_list = [
+    {'min_salary': 2300, 'max_salary': 25000, 'date_posted': '2019-01-19'},
+    {'min_salary': 1500, 'max_salary': 22000, 'date_posted': '2020-04-19'},
+    {'min_salary': 3000, 'max_salary': 50000, 'date_posted': '2021-01-21'},
+]
+
+max_salary_list = [
+    {'min_salary': 2300, 'max_salary': 25000, 'date_posted': '2019-01-19'},
+    {'min_salary': 3000, 'max_salary': 50000, 'date_posted': '2021-01-21'},
+    {'min_salary': 1500, 'max_salary': 22000, 'date_posted': '2020-04-19'},
+]
+
+date_posted_list = [
+    {'min_salary': 3000, 'max_salary': 50000, 'date_posted': '2021-01-21'},
+    {'min_salary': 1500, 'max_salary': 22000, 'date_posted': '2020-04-19'},
+    {'min_salary': 2300, 'max_salary': 25000, 'date_posted': '2019-01-19'},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    assert sort_by(list, 'min_salary') == min_salary_list
+    assert sort_by(list, 'max_salary') == max_salary_list
+    assert sort_by(list, 'date_posted') == date_posted_list
