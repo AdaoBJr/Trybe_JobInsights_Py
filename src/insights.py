1d07b79312699a2560f91b774f65f071ab0ab71e
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-12-09 20:57:57 -0300

    Change SET() for []

--- src/insights.py
+++ src/insights.py
@@ -3,10 +3,10 @@ from src.jobs import read
 
 def get_unique_job_types(path):
     jobs = read(path)
-    job_types = set()
+    job_types = []
     for job in jobs:
         if job["job_type"] not in job_types:
-            job_types.add(job["job_type"])
+            job_types.append(job["job_type"])
     return job_types
 
 
@@ -37,45 +37,24 @@ def filter_by_industry(jobs, industry):
 
 def get_max_salary(path):
     jobs = read(path)
-    max_salary = set()
+    max_salary = []
     for element in jobs:
         if element["max_salary"] != "invalid" and element["max_salary"] != "":
-            max_salary.add(int(element["max_salary"]))
+            max_salary.append(int(element["max_salary"]))
     return max(max_salary)
 
 
 def get_min_salary(path):
     jobs = read(path)
-    min_salary = set()
+    min_salary = []
     for element in jobs:
         if element["min_salary"] != "invalid" and element["min_salary"] != "":
-            min_salary.add(int(element["min_salary"]))
+            min_salary.append(int(element["min_salary"]))
     return min(min_salary)
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
     pass
 
 
