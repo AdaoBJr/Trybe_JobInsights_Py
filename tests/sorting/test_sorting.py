32bc1e4d700bd265d1bf1b4d1f102ac360e4771c
tfeliperibeiro <thiagoribeiromixtv@gmail.com>
2021-12-09 13:04:20 -0300

    Requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,17 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+salary = [
+    {"max_salary": 10000, "min_salary": 200, "date_posted": "2020-02-03"},
+    {"max_salary": 1500, "min_salary": 0, "date_posted": "2020-02-04"},
+    {"max_salary": 1600, "min_salary": 9, "date_posted": "2020-02-01"},
+]
+
+min_salary = [
+    {"max_salary": 10000, "min_salary": 200, "date_posted": "2020-02-03"},
+    {"max_salary": 1500, "min_salary": 0, "date_posted": "2020-02-04"},
+    {"max_salary": 1600, "min_salary": 9, "date_posted": "2020-02-01"},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    assert sort_by(salary, "max_salary") is min_salary
