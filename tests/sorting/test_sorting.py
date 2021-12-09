dbdd90489a2df10fe2af8be7cb361419b1446726
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-12-09 07:07:26 -0300

    Resolvido o requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,10 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    assert (
+        sort_by([
+            {"job_title": "Full Stack Developer"},
+            {"company": "Trybe"}
+        ], "min_salary") is None
+    )
