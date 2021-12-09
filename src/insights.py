79362c4d5d931fbc69ee45fecd8c45e71879bd62
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-12-09 14:49:53 -0300

    get_unique_industries function

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    file = read(path)
+    industries = set()
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    for row in file:
+        if row["industry"] != '':
+            industries.add(row["industry"])
+    return industries
 
 
 def filter_by_industry(jobs, industry):
