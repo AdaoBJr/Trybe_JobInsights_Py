dee19569e7eda3cf43daed06eadccc18a7a64b7d
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-12-09 20:53:09 -0300

    Feat: Requisito nove

--- src/insights.py
+++ src/insights.py
@@ -66,18 +66,12 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
+    filter_salary = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filter_salary.append(job)
+        except ValueError:
+            pass
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    return filter_salary
