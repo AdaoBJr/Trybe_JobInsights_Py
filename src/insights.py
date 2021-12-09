3caede2aae7e7b46dd9a4681be8262f3de229be5
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-12-09 20:12:42 -0300

    Done req 3

--- src/insights.py
+++ src/insights.py
@@ -3,47 +3,24 @@ from src.jobs import read
 
 def get_unique_job_types(path):
     jobs = read(path)
-    job_types = []
+    job_types = set()
     for job in jobs:
         if job["job_type"] not in job_types:
-            job_types.append(job["job_type"])
+            job_types.add(job["job_type"])
     return job_types
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
     return []
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    jobs = read(path)
+    industries = []
+    for element in jobs:
+        if element["industry"] not in industries:
+            industries.append(element["industry"])
+    return industries
 
 
 def filter_by_industry(jobs, industry):
