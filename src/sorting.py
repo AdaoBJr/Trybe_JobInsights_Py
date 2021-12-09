6f7f122ff544f8ca79a7436c257a214677939a07
Nícolas Tozato <ntozato9@gmail.com>
2021-12-08 21:17:19 -0300

    requisito 10

--- src/sorting.py
+++ src/sorting.py
@@ -94,3 +94,36 @@ def sort_by(jobs, criteria):
     reverse = criteria in ["max_salary", "date_posted"]
 
     jobs.sort(key=key, reverse=reverse)
+
+
+# if __name__ == '__main__':
+#     job_1 = {
+#         "job_title": "backend dev",
+#         "min_salary": 3000,
+#         "max_salary": 10000,
+#         "date_posted": "2021-08-12"
+#     }
+
+#     job_2 = {
+#         "job_title": "frontend dev",
+#         "min_salary": 2500,
+#         "max_salary": 9000,
+#         "date_posted": "2021-04-03"
+#     }
+
+#     job_3 = {
+#         "job_title": "java dev",
+#         "min_salary": 3500,
+#         "max_salary": 12000,
+#         "date_posted": "2021-03-08"
+#     }
+
+#     jobs = [job_1, job_2, job_3]
+
+#     result_by_min_salary = [job_2, job_1, job_3]
+#     result_by_max_salary = [job_3, job_1, job_2]
+#     result_by_date_posted = [job_1, job_2, job_3]
+
+#     teste = sort_by(jobs, 'max_salary')
+#     print(teste)
+#     print(jobs[0])
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,40 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+
+    job_1 = {
+        "job_title": "backend dev",
+        "min_salary": 3000,
+        "max_salary": 10000,
+        "date_posted": "2021-08-12"
+    }
+
+    job_2 = {
+        "job_title": "frontend dev",
+        "min_salary": 2500,
+        "max_salary": 9000,
+        "date_posted": "2021-04-03"
+    }
+
+    job_3 = {
+        "job_title": "java dev",
+        "min_salary": 3500,
+        "max_salary": 12000,
+        "date_posted": "2021-03-08"
+    }
+
+    jobs = [job_1, job_2, job_3]
+
+    result_by_min_salary = [job_2, job_1, job_3]
+    result_by_max_salary = [job_3, job_1, job_2]
+    result_by_date_posted = [job_1, job_2, job_3]
+
+    sort_by(jobs, "min_salary")
+    assert jobs == result_by_min_salary
+
+    sort_by(jobs, "max_salary")
+    assert jobs == result_by_max_salary
+
+    sort_by(jobs, "date_posted")
+    assert jobs == result_by_date_posted
