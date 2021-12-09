7e35f876d111c8b23c9b929f5934c6b20802d236
tfeliperibeiro <thiagoribeiromixtv@gmail.com>
2021-12-09 13:14:51 -0300

    Corrigindo min_salary

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -7,11 +7,11 @@ salary = [
 ]
 
 min_salary = [
-    {"max_salary": 10000, "min_salary": 200, "date_posted": "2020-02-03"},
     {"max_salary": 1500, "min_salary": 0, "date_posted": "2020-02-04"},
     {"max_salary": 1600, "min_salary": 9, "date_posted": "2020-02-01"},
+    {"max_salary": 10000, "min_salary": 200, "date_posted": "2020-02-03"},
 ]
 
 
 def test_sort_by_criteria():
-    assert sort_by(salary, "max_salary") == min_salary
+    assert sort_by(salary, "min_salary") is min_salary
