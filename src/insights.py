c8ae5b3df962ac3123f97902e8b154b701c6b202
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-12-09 15:14:04 -0300

    Implemented the 'filter_by_job_type' function in 'insights.py'

--- src/insights.py
+++ src/insights.py
@@ -40,7 +40,11 @@ def filter_by_job_type(jobs, job_type):
     list
         List of jobs with provided job_type
     """
-    return []
+    filtered_jobs = [
+        job for job in jobs
+        if job['job_type'] == job_type]
+
+    return filtered_jobs
 
 
 def get_unique_industries(path):
