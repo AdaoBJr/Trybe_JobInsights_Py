c2129f9f6deadf82033f354205924960cbb0cc94
Alan Tanaka <alan.tanaka@ubistart.com>
2021-12-09 16:32:17 -0300

    up

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,15 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.insights import get_min_salary
+from src.jobs import read
+
+# ME AUXILIEI NO CÓDIGO DE ANDRE ARNONI TURMA 10 - B
 
 
 def test_sort_by_criteria():
-    pass
+    jobs_file = read("src/jobs.csv")
+
+    sort_by(jobs_file, "min_salary")
+    sorted = jobs_file[0]["min_salary"]
+
+    assert type(jobs_file) is list
+    assert sorted == str(get_min_salary("src/jobs.csv"))
