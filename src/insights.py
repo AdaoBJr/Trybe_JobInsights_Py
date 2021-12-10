2de23774deb0f5a685347bacb4784dc7c3c5d05e
andersontrkz <trkz.anderson@gmail.com>
2021-12-09 23:48:31 -0300

    Requisito 5 - Implemente a função

--- src/insights.py
+++ src/insights.py
@@ -38,21 +38,12 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    all_jobs = read(path)
+    max_salary = []
+    for job in all_jobs:
+        if job["max_salary"] != '' and job["max_salary"] != 'invalid':
+            max_salary.append(int(job["max_salary"]))
+    return max(max_salary)
 
 
 def get_min_salary(path):
