212e3a849842fa3bc11aa517a5cf9adc4d7ddd00
andersontrkz <trkz.anderson@gmail.com>
2021-12-10 02:12:29 -0300

    Requisito 10 - Implemente um teste para a função

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,20 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+from src.jobs import read
+from src.insights import get_min_salary
+from src.insights import get_max_salary
 
 
 def test_sort_by_criteria():
-    pass
+    path = 'src/jobs.csv'
+    jobs = read(path)
+
+    assert type(jobs) == list
+
+    sort_by(jobs, 'min_salary')
+    assert jobs[0]['min_salary'] == str(get_min_salary(path))
+
+    sort_by(jobs, 'max_salary')
+    assert jobs[0]['max_salary'] == str(get_max_salary(path))
+
+    sort_by(jobs, 'date_posted')
+    assert jobs[0]['date_posted']
