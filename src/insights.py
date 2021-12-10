bfaaaa4be6eb984a072dca04bf2fd8635c37bdaa
Pablo Fernandes <pablofernandes@Owners-MacBook-Pro.local>
2021-12-09 21:43:53 -0500

    Requirement three - Done

--- src/insights.py
+++ src/insights.py
@@ -55,7 +55,11 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+
+    jobs_list = read(path)
+    unique_industries = {
+        job["industry"] for job in jobs_list if job["industry"] != ""}
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):
