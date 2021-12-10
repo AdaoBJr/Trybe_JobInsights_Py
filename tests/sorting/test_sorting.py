778fe22b8cfd0da057e9e0aaded957d61ca5c983
Henrique Clementino <strikedv@hotmail.com>
2021-12-10 01:13:34 -0300

    agora vai

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,25 +1,26 @@
 from src.sorting import sort_by
 
 
-jobs_default = [
-    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
-    {"min_salary": 1000, "max_salary": 4000, "date_posted": "2019-01-01"},
-    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-01-01"},
+default_list = [
+    {"min_salary": 500, "max_salary": 800, "date_posted": "2021-12-09"},
+    {"min_salary": 400, "max_salary": 700, "date_posted": "2021-11-09"},
+    {"min_salary": 600, "max_salary": 900, "date_posted": "2021-10-09"},
 ]
 
 min_salary = [
-    {"min_salary": 1000, "max_salary": 4000, "date_posted": "2019-01-01"},
-    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
-    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-01-01"},
+    {"min_salary": 400, "max_salary": 700, "date_posted": "2021-11-09"},
+    {"min_salary": 500, "max_salary": 800, "date_posted": "2021-12-09"},
+    {"min_salary": 600, "max_salary": 900, "date_posted": "2021-10-09"},
 ]
 
-max_salary = [
-    {"min_salary": 3000, "max_salary": 6000, "date_posted": "2020-01-01"},
-    {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-01-01"},
-    {"min_salary": 1000, "max_salary": 4000, "date_posted": "2019-01-01"},
+max_salary_or_data_posted = [
+    {"min_salary": 600, "max_salary": 900, "date_posted": "2021-10-09"},
+    {"min_salary": 500, "max_salary": 800, "date_posted": "2021-12-09"},
+    {"min_salary": 400, "max_salary": 700, "date_posted": "2021-11-09"},
 ]
 
 
 def test_sort_by_criteria():
-    assert sort_by(jobs_default, "min_salary") == min_salary
-    assert sort_by(jobs_default, "max_salary") == max_salary
+    assert sort_by(default_list, "min_salary") == min_salary
+    assert sort_by(default_list, "max_salary") == max_salary_or_data_posted
+    assert sort_by(default_list, "date_posted") == max_salary_or_data_posted
