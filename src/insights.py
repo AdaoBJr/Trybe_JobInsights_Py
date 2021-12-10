5d13abb988b87c8015397d473c64222058665571
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-12-09 23:00:22 -0300

    :sparkles: feat: Terceiro requisito :sparkles:

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    read_file = read(path)
+    unique_industries = []
+    for row in read_file:
+        if row["industry"] not in unique_industries and row["industry"] != "":
+            unique_industries.append(row["industry"])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):
