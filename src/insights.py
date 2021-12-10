5e9ce7f541c2d8d0fb8655b4717a27fd0aa19ebb
andredebem <andrebemferraz@gmail.com>
2021-12-09 23:34:37 -0300

    Request V

--- src/insights.py
+++ src/insights.py
@@ -71,21 +71,14 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
+    jobs_list = read(path)
+    min_salaries = []
 
-    Must call `read`
+    for job in jobs_list:
+        if job["min_salary"].isnumeric():
+            min_salaries.append(int(job["min_salary"]))
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    return min(min_salaries)
 
 
 def matches_salary_range(job, salary):
