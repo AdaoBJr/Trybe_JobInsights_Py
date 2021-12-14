dad01ab0c41c3c873d9328d0004604dccbfdab79
antonioarieiro <sfgfffs@gmail.com>
2021-12-13 21:56:00 -0300

    using blue print for routes in jobs

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -11,7 +11,7 @@ from .insights import (
     get_min_salary,
     get_max_salary,
 )
-from .more_insights import slice_jobs, get_int_from_args, build_jobs_urls
+from .more_insights import slice_jobs, get_int_from_args, build_jobs_urls, get_job,
 
 bp = Blueprint("client", __name__, template_folder="templates")
 
@@ -57,6 +57,15 @@ def list_jobs():
     }
 
     return render_template("list_jobs.jinja2", ctx=ctx)
+
+
+@bp.route("/job/<index>")
+# https://python.hotexamples.com/examples/flask/Blueprint/route/python-blueprint-route-method-examples.html
+# https://stackoverflow.com/questions/62735758/flask-routes-using-blueprint-are-not-working-only-route-is-working-on-ubunt
+def job(index):
+    jobs = read("src/jobs.csv")
+    job = get_job(jobs, index)
+    return render_template("job.jinja2", job=job)
 
 
 def init_app(app: Flask):
