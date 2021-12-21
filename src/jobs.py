9fdbb09a9ee659cfdeb287be9555235da376372a
root <diegoserafim1@gmail.com>
2021-12-21 17:45:12 -0300

    1 - Lê um arquivo de um determinado caminho e retorna seu conteúdo

--- src/jobs.py
+++ src/jobs.py
@@ -1,10 +1,17 @@
 from functools import lru_cache
+import csv
+# Lê um arquivo de um determinado caminho e retorna seu conteúdo
+# Parâmetros
+# caminho : str
+# Caminho completo para arquivar
+# Retorna
+#  lista
+# Lista de linhas como dicts
 
 
 @lru_cache
 def read(path):
     """Reads a file from a given path and returns its contents
-
     Parameters
     ----------
     path : str
@@ -15,4 +22,10 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path) as file:
+        path_file = csv.DictReader(file)
+        list = []
+        for row in path_file:
+            list.append(row)
+
+    return list
