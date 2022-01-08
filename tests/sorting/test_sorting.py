7873afd20a9fb79089094d9d9d73be425b38c2c9
Abimael <abimaelr.albu@gmail.com>
2022-01-08 09:35:52 -0300

    pytest

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,23 +1,23 @@
 from src.sorting import sort_by
-
+import pytest
 
 job_list = [
     {
         "job": "Driver",
-        "min_salary": "500",
-        "max_salary": "800",
+        "min_salary": 500,
+        "max_salary": 800,
         "date_posted": "2021-02-09",
     },
     {
         "job": "Teacher",
-        "min_salary": "800",
-        "max_salary": "1000",
+        "min_salary": 800,
+        "max_salary": 1000,
         "date_posted": "2020-02-09",
     },
     {
         "job": "Teacher",
-        "min_salary": "800",
-        "max_salary": "1200",
+        "min_salary": 900,
+        "max_salary": 1200,
         "date_posted": "2022-02-09",
     },
 ]
@@ -74,5 +74,6 @@ def test_sort_by_criteria():
 
     sort_by(job_list, "date_posted")
     assert job_list == sorted_max_salary_date_posted
-    # with pytest.raises(ValueError):
-    #     sort_by(job_list, 'anything')
+
+    with pytest.raises(ValueError):
+        sort_by(job_list, 'anything')
