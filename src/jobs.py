f733765920db6698009c5d7db0195d023b663de4
Abimael <abimaelr.albu@gmail.com>
2022-01-08 09:30:07 -0300

    sort_by

--- src/jobs.py
+++ src/jobs.py
@@ -7,3 +7,4 @@ def read(path):
     with open(path, mode='r') as csv_file:
         csv_list = list(csv.DictReader(csv_file, delimiter=','))
     return csv_list
+
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,78 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+
+job_list = [
+    {
+        "job": "Driver",
+        "min_salary": "500",
+        "max_salary": "800",
+        "date_posted": "2021-02-09",
+    },
+    {
+        "job": "Teacher",
+        "min_salary": "800",
+        "max_salary": "1000",
+        "date_posted": "2020-02-09",
+    },
+    {
+        "job": "Teacher",
+        "min_salary": "800",
+        "max_salary": "1200",
+        "date_posted": "2022-02-09",
+    },
+]
+
+sorted_min_salary = [
+    {
+        "job": "Driver",
+        "min_salary": "500",
+        "max_salary": "800",
+        "date_posted": "2021-02-09",
+    },
+    {
+        "job": "Teacher",
+        "min_salary": "800",
+        "max_salary": "1000",
+        "date_posted": "2020-02-09",
+    },
+    {
+        "job": "Teacher",
+        "min_salary": "800",
+        "max_salary": "1200",
+        "date_posted": "2022-02-09",
+    },
+]
+
+sorted_max_salary_date_posted = [
+    {
+        "job": "Teacher",
+        "min_salary": "800",
+        "max_salary": "1200",
+        "date_posted": "2022-02-09",
+    },
+    {
+        "job": "Teacher",
+        "min_salary": "800",
+        "max_salary": "1000",
+        "date_posted": "2021-02-09",
+    },
+    {
+        "job": "Driver",
+        "min_salary": "500",
+        "max_salary": "800",
+        "date_posted": "2020-02-09",
+    },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(job_list, "min_salary")
+    assert job_list == sorted_min_salary
+
+    sort_by(job_list, "max_salary")
+    assert job_list == sorted_max_salary_date_posted
+
+    sort_by(job_list, "date_posted")
+    assert job_list == sorted_max_salary_date_posted
+    # with pytest.raises(ValueError):
+    #     sort_by(job_list, 'anything')
