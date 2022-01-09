c2acc823d062f4b9a4ead3cd40bd383c4ac58951
Letycia Barreto <letyciasbarreto@gmail.com>
2022-01-09 18:38:23 -0300

    req 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,30 +1,30 @@
 from src.sorting import sort_by
 
 jobs = [
-    {"min_salary": 3000, "max_salary": 5500, "date_posted": "2021-11-09"},
-    {"min_salary": 1000, "max_salary": 3500, "date_posted": "2020-01-19"},
-    {"min_salary": 2000, "max_salary": 5500, "date_posted": "2022-01-09"}
+    {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-01-09"},
+    {"min_salary": 2000, "max_salary": 3000, "date_posted": "2021-01-09"},
+    {"min_salary": 3000, "max_salary": 4000, "date_posted": "2022-01-09"},
 ]
 
 
 def test_sort_by_criteria():
     sort_by(jobs, "min_salary")
     assert jobs == [
-        {"min_salary": 3000, "max_salary": 5500, "date_posted": "2021-11-09"},
-        {"min_salary": 1000, "max_salary": 3500, "date_posted": "2020-01-19"},
-        {"min_salary": 2000, "max_salary": 5500, "date_posted": "2022-01-09"}
+        {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-01-09"},
+        {"min_salary": 2000, "max_salary": 3000, "date_posted": "2021-01-09"},
+        {"min_salary": 3000, "max_salary": 4000, "date_posted": "2022-01-09"},
     ]
 
     sort_by(jobs, "max_salary")
     assert jobs == [
-        {"min_salary": 3000, "max_salary": 5500, "date_posted": "2021-11-09"},
-        {"min_salary": 1000, "max_salary": 3500, "date_posted": "2020-01-19"},
-        {"min_salary": 2000, "max_salary": 5500, "date_posted": "2022-01-09"}
+        {"min_salary": 3000, "max_salary": 4000, "date_posted": "2022-01-09"},
+        {"min_salary": 2000, "max_salary": 3000, "date_posted": "2021-01-09"},
+        {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-01-09"},
     ]
 
-    sort_by(jobs, "data_posted")
+    sort_by(jobs, "date_posted")
     assert jobs == [
-        {"min_salary": 3000, "max_salary": 5500, "date_posted": "2021-11-09"},
-        {"min_salary": 1000, "max_salary": 3500, "date_posted": "2020-01-19"},
-        {"min_salary": 2000, "max_salary": 5500, "date_posted": "2022-01-09"}
+        {"min_salary": 3000, "max_salary": 4000, "date_posted": "2022-01-09"},
+        {"min_salary": 2000, "max_salary": 3000, "date_posted": "2021-01-09"},
+        {"min_salary": 1000, "max_salary": 2000, "date_posted": "2020-01-09"}
     ]
