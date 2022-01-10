a6bc0eec0b963a90f72017d203d6bd94b198e409
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2022-01-10 19:05:17 -0300

    Fix: require 10 and require 11

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -66,10 +66,10 @@ list_desc_by_max = [
 
 list_desc_by_date = [
     {
-        "min_salary": 2000,
-        "max_salary": 4000,
-        "date_posted": "2022-01-01",
-        "status": "SP",
+        "min_salary": 5000,
+        "max_salary": 10000,
+        "date_posted": "2022-01-03",
+        "status": "MA",
     },
     {
         "min_salary": 1000,
@@ -78,18 +78,21 @@ list_desc_by_date = [
         "status": "RJ",
     },
     {
-        "min_salary": 5000,
-        "max_salary": 10000,
-        "date_posted": "2022-01-03",
-        "status": "MA",
+        "min_salary": 2000,
+        "max_salary": 4000,
+        "date_posted": "2022-01-01",
+        "status": "SP",
     },
 ]
 
 
 def test_sort_by_criteria():
 
-    assert sort_by(list_dicts, "min_salary") == list_asc_by_min
+    sort_by(list_dicts, "min_salary")
+    assert list_dicts == list_asc_by_min
 
-    assert sort_by(list_dicts, "max_salary") == list_desc_by_max
+    sort_by(list_dicts, "max_salary")
+    assert list_dicts == list_desc_by_max
 
-    assert sort_by(list_dicts, "date_posted") == list_desc_by_date
+    sort_by(list_dicts, "date_posted")
+    assert list_dicts == list_desc_by_date
