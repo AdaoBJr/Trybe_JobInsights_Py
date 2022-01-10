6d28cc2fceac87a9accca1ecfd9fd3858e0b6447
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2022-01-10 13:46:47 -0300

    Fix: require 11

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -2,33 +2,94 @@ from src.sorting import sort_by
 
 
 list_dicts = [
-    {"min_salary": 2000, "max_salary": 4000, "date_posted": "2022-01-01"},
-    {"min_salary": 1000, "max_salary": 3000, "date_posted": "2022-01-02"},
-    {"min_salary": 5000, "max_salary": 10000, "date_posted": "2022-01-03"},
+    {
+        "min_salary": 2000,
+        "max_salary": 4000,
+        "date_posted": "2022-01-01",
+        "status": "SP",
+    },
+    {
+        "min_salary": 1000,
+        "max_salary": 3000,
+        "date_posted": "2022-01-02",
+        "status": "RJ",
+    },
+    {
+        "min_salary": 5000,
+        "max_salary": 10000,
+        "date_posted": "2022-01-03",
+        "status": "MA",
+    },
 ]
 
-list_desc_by_min = [
-    {"min_salary": 1000, "max_salary": 3000, "date_posted": "2022-01-02"},
-    {"min_salary": 2000, "max_salary": 4000, "date_posted": "2022-01-01"},
-    {"min_salary": 5000, "max_salary": 10000, "date_posted": "2022-01-03"},
+list_asc_by_min = [
+    {
+        "min_salary": 1000,
+        "max_salary": 3000,
+        "date_posted": "2022-01-02",
+        "status": "RJ",
+    },
+    {
+        "min_salary": 2000,
+        "max_salary": 4000,
+        "date_posted": "2022-01-01",
+        "status": "SP",
+    },
+    {
+        "min_salary": 5000,
+        "max_salary": 10000,
+        "date_posted": "2022-01-03",
+        "status": "MA",
+    },
 ]
 
 list_desc_by_max = [
-    {"min_salary": 5000, "max_salary": 10000, "date_posted": "2022-01-03"},
-    {"min_salary": 2000, "max_salary": 4000, "date_posted": "2022-01-01"},
-    {"min_salary": 1000, "max_salary": 3000, "date_posted": "2022-01-02"},
+    {
+        "min_salary": 5000,
+        "max_salary": 10000,
+        "date_posted": "2022-01-03",
+        "status": "MA",
+    },
+    {
+        "min_salary": 2000,
+        "max_salary": 4000,
+        "date_posted": "2022-01-01",
+        "status": "SP",
+    },
+    {
+        "min_salary": 1000,
+        "max_salary": 3000,
+        "date_posted": "2022-01-02",
+        "status": "RJ",
+    },
 ]
 
 list_desc_by_date = [
-    {"min_salary": 2000, "max_salary": 4000, "date_posted": "2022-01-01"},
-    {"min_salary": 1000, "max_salary": 3000, "date_posted": "2022-01-02"},
-    {"min_salary": 5000, "max_salary": 10000, "date_posted": "2022-01-03"},
+    {
+        "min_salary": 2000,
+        "max_salary": 4000,
+        "date_posted": "2022-01-01",
+        "status": "SP",
+    },
+    {
+        "min_salary": 1000,
+        "max_salary": 3000,
+        "date_posted": "2022-01-02",
+        "status": "RJ",
+    },
+    {
+        "min_salary": 5000,
+        "max_salary": 10000,
+        "date_posted": "2022-01-03",
+        "status": "MA",
+    },
 ]
 
 
 def test_sort_by_criteria():
-    assert sort_by(list_dicts, 'min_salary') == list_desc_by_min
 
-    assert sort_by(list_dicts, 'max_salary') == list_desc_by_max
+    assert sort_by(list_dicts, "min_salary") == list_asc_by_min
 
-    assert sort_by(list_dicts, 'date_posted') == list_desc_by_date
+    assert sort_by(list_dicts, "max_salary") == list_desc_by_max
+
+    assert sort_by(list_dicts, "date_posted") == list_desc_by_date
