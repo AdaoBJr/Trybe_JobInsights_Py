e36c0992b991df9c54ef9a997b73b8611be30d97
Matheus H. Gois <matheus.hengo@gmail.com>
2022-01-11 15:39:22 -0300

    feat: creates test_sort_by_criteria

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,64 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+jobs_list = [
+    {
+        "max_salary": 2000,
+        "min_salary": 1000,
+        "date_posted": "2022-01-11",
+    },
+    {
+        "max_salary": 4000,
+        "min_salary": 2000,
+        "date_posted": "2022-01-12",
+    },
+]
+
+min_salary_order = [
+    {
+        "max_salary": 2000,
+        "min_salary": 1000,
+        "date_posted": "2022-01-11",
+    },
+    {
+        "max_salary": 4000,
+        "min_salary": 2000,
+        "date_posted": "2022-01-12",
+    },
+]
+
+max_salary_order = [
+    {
+        "max_salary": 4000,
+        "min_salary": 2000,
+        "date_posted": "2022-01-12",
+    },
+    {
+        "max_salary": 2000,
+        "min_salary": 1000,
+        "date_posted": "2022-01-11",
+    },
+]
+
+date_posted_order = [
+    {
+        "max_salary": 4000,
+        "min_salary": 2000,
+        "date_posted": "2022-01-12",
+    },
+    {
+        "max_salary": 2000,
+        "min_salary": 1000,
+        "date_posted": "2022-01-11",
+    },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs_list, "min_salary")
+    assert jobs_list == min_salary_order
+
+    sort_by(jobs_list, "max_salary")
+    assert jobs_list == max_salary_order
+
+    sort_by(jobs_list, "date_posted")
+    assert jobs_list == date_posted_order
