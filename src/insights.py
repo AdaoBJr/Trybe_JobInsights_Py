accd056e9c5db858f05107523f3d478c13c24765
Matheus H. Gois <matheus.hengo@gmail.com>
2022-01-11 14:31:25 -0300

    feat: creates get_min_salary

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,13 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    jobs_list = read(path)
+    return min([
+      int(job["min_salary"])
+      for job
+      in jobs_list
+      if job["min_salary"] and job["min_salary"].isnumeric()
+    ])
 
 
 def matches_salary_range(job, salary):
