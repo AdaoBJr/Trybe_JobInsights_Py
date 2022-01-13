d4af0e8616ae5d1631a71863f51799f505cd9a27
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2022-01-12 22:51:43 -0300

    Req 10.4 teste

--- src/sorting.py
+++ src/sorting.py
@@ -94,3 +94,5 @@ def sort_by(jobs, criteria):
     reverse = criteria in ["max_salary", "date_posted"]
 
     jobs.sort(key=key, reverse=reverse)
+
+    print(jobs)
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,45 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+job_list = [
+        {
+            "min_salary": 10,
+            "max_salary": 100,
+            "date_posted": "14-02-1997"
+        },
+        {
+            "min_salary": 1000,
+            "max_salary": 999999,
+            "date_posted": "22-11-2004"
+        },
+        {
+            "min_salary": 100,
+            "max_salary": 1000,
+            "date_posted": "02-06-2007"
+        },
+    ]
+
+max_salary_sort = [
+        {
+            "min_salary": 1000,
+            "max_salary": 999999,
+            "date_posted": "22-11-2004"
+        },
+        {
+            "min_salary": 100,
+            "max_salary": 1000,
+            "date_posted": "02-06-2007"
+        },
+        {
+            "min_salary": 10,
+            "max_salary": 100,
+            "date_posted": "14-02-1997"
+        }
+    ]
 
 
 def test_sort_by_criteria():
-    pass
+
+    new_job_list = job_list
+    sort_by(new_job_list, "max_salary")
+
+    assert new_job_list == max_salary_sort
