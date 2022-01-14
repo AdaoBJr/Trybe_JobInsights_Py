4f7eaad5b99fdbccc565e7e858d7358e16a0805f
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2022-01-13 21:08:53 -0300

    Req 11 teste

--- src/insights.py
+++ src/insights.py
@@ -8,25 +8,11 @@ def get_unique_job_types(path):
     job_types = set()
     for job in job_list:
         job_types.add(job["job_type"])
-    # uniqueValues = job_list['job_type'].unique()
     return job_types
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
     jobsFilter = []
     for job in jobs:
         if job["job_type"] == job_type:
